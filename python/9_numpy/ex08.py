c4cf2f5782a6bc842da4ad917cd88229ccf33b8a
rajesh <rajeshsurya1994@gmail.com>
2019-02-01 16:22:20 +0530

    Python excercise in numpy on 01-02-2019

--- python/9_numpy/ex08.py
+++ python/9_numpy/ex08.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 # Initialize 'x' & 'y'
-x = np.arange(3.0)
+x = np.arange(9.0).reshape(3, 3)
 y = np.arange(3.0)
 print("Array 'x' is:")
 print(x)
@@ -14,6 +14,9 @@ print(y)
 # Adding two array using np.add() method
 add = np.add(x, y)
 print("\nAddition of two arrays:")
-print(x)
+print(add)
 
 # Subtracting two array using np.subtract() method
+sub = np.subtract(x, y)
+print("\nSubtraction of two arrays:")
+print(sub)
