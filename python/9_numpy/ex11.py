763a0f41711f966f8bdc885cffb41b8ab1a09e75
rajesh <rajeshsurya1994@gmail.com>
2019-02-04 15:53:17 +0530

    Python excercise in numpy on 04-02-2019

--- python/9_numpy/ex11.py
+++ python/9_numpy/ex11.py
@@ -25,3 +25,15 @@ print(x[1][2])
 # Select the element at row 1 colomn 2
 print("\nPrinting the element at row 1 colomn 2:")
 print(x[1, 2])
+
+# Numpy's slicing examples
+
+# Select items at index 0 & 1
+print("\nPrinting item at index 0 & 1:")
+print(x[0 : 2])
+
+# Select items at row 0 & 1, colomn 1
+print("\nPrinting items at row 0 & 1, colomn 1:")
+print(x[0 : 2, 1])
+
+# 
