e53d87f17c3ec8a1ff5437332ddbed54a962018e
rajesh <rajeshsurya1994@gmail.com>
2019-02-04 14:40:51 +0530

    Python excercise in numpy on 04-02-2019

---
+++ python/9_numpy/ex10.py
@@ -0,0 +1,14 @@
+# Numpy's logical operations
+
+# Import numpy as 'np'
+import numpy as np
+
+# Initialize 'x' & 'y'
+x = np.array([[2, 3, 4], [7, 8, 22], [44, 22, 38]])
+y = np.array([[19, 7, 5], [37, 31, 83], [26, 53, 101]])
+
+# Print 'x' & 'y'
+print("Array of 'x' is:")
+print(x)
+print("Array of 'y' is:")
+print(y)
