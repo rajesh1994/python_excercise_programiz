2523503c2b63e82c59f363c6df47631808908c7d
rajesh <rajeshsurya1994@gmail.com>
2019-02-05 13:12:38 +0530

    Python excercise in numpy on 05-02-2019

--- python/9_numpy/ex15.py
+++ python/9_numpy/ex15.py
@@ -10,12 +10,15 @@ print(x1)
 
 # Appending 1D array to array 'x1'
 x2 = np.append(x1, [12, 14, 16, 18])
-print("After appending 1D array to array x1:")
+print("\nAfter appending 1D array to array x1:")
 print(x2)
 
 # Initialize array 'y1'
 y1 = np.array([[2,3], [4, 5]])
-print("Array 'y1' is:")
+print("\nArray 'y1' is:")
 print(y1)
 
-# Appending
+# Appending an extra colomn to array y1
+y2 = np.append(y1, [[8], [6]], axis = 1)
+print("\nAfter appending an extra colomn to array y1:")
+print(y2)
