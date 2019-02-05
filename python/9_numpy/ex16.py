29d59274707aa663ec182d7aeb8744aa46d823dc
rajesh <rajeshsurya1994@gmail.com>
2019-02-05 14:21:11 +0530

    Python excercise in numpy on 05-02-2019

---
+++ python/9_numpy/ex16.py
@@ -0,0 +1,18 @@
+# How To Insert And Delete Array Elements
+
+# Import numpy as 'np'
+import numpy as np
+
+# Initialize array 'x1'
+x1 = np.array([[1, 5, 9, 3], [2, 6, 10, 4]])
+print("Array 'x1' is:")
+print(x1)
+
+# Insert 7 at index 1
+print("\nInserting element 7 in index 1:")
+print(np.insert(x1, [1], 22))
+
+# Initialize array 'y1'
+y1 = np.array([[3, 4], [11, 22]])
+print("\nArray 'y1' is:")
+print(y1)
