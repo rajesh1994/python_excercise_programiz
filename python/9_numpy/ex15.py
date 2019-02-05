e3820c723bee5090e22f85d504c1e09422477f28
rajesh <rajeshsurya1994@gmail.com>
2019-02-05 13:07:21 +0530

    Python excercise in numpy on 05-02-2019

---
+++ python/9_numpy/ex15.py
@@ -0,0 +1,21 @@
+# How To Append Arrays
+
+# Import numpy as 'np'
+import numpy as np
+
+# Initialize array 'x1'
+x1 = np.array([[7, 5, 9, 3], [2, 3, 4, 5]])
+print("Array 'x1' is:")
+print(x1)
+
+# Appending 1D array to array 'x1'
+x2 = np.append(x1, [12, 14, 16, 18])
+print("After appending 1D array to array x1:")
+print(x2)
+
+# Initialize array 'y1'
+y1 = np.array([[2,3], [4, 5]])
+print("Array 'y1' is:")
+print(y1)
+
+# Appending
