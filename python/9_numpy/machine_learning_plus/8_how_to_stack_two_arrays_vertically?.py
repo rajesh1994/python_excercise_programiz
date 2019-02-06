0510a4b870c42c31f59942cd73543367911dc474
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 13:12:46 +0530

    Python excercise in numpy on 06-02-2019

--- python/9_numpy/machine_learning_plus/8_how_to_stack_two_arrays_vertically?.py
+++ python/9_numpy/machine_learning_plus/8_how_to_stack_two_arrays_vertically?.py
@@ -5,4 +5,10 @@ Problem Statement : How to stack two arrays vertically?
 # Import numpy as 'np'
 import numpy as np
 
-# Initialize arrays 'x' &
+# Initialize arrays 'x' & 'y'
+x = np.arange(10).reshape(2, -1)
+y = np.repeat(1, 10).reshape(2, -1)
+print("Array 'x' is:")
+print(x)
+print("\nArray 'y' is:")
+print(y)
