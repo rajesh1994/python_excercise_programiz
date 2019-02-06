a66ec1e8474979deffdc6e8e77ed1c3a1a595e30
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 11:19:07 +0530

    Python excercise in numpy on 06-02-2019

---
+++ python/9_numpy/machine_learning_plus/1_import_numpy_as_np_and_see_the_version.py
@@ -0,0 +1,8 @@
+"""
+Problem Statement : Import numpy as np and see the version
+"""
+
+# Import numpy as 'np'
+import numpy as np
+print("Numpy version is:")
+print(np.__version__)
---
+++ python/9_numpy/machine_learning_plus/2_how_to_create_a_1d_array?.py
@@ -0,0 +1,13 @@
+"""
+Problem Statement : How to create a 1D array?
+"""
+
+# Import numpy as 'np'
+import numpy as np
+
+# Creating 1D array by using arange() method
+one_d_array = np.arange(10)
+
+# Printing 1D array
+print("Printing 1D array:")
+print(one_d_array)
