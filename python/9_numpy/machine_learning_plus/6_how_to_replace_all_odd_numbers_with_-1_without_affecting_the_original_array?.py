746f576152f6b33738c34927cef3c444597db7f0
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 12:41:28 +0530

    Python excercise in numpy on 06-02-2019

--- python/9_numpy/machine_learning_plus/6_how_to_replace_all_odd_numbers_with_-1_without_affecting_the_original_array?.py
+++ python/9_numpy/machine_learning_plus/6_how_to_replace_all_odd_numbers_with_-1_without_affecting_the_original_array?.py
@@ -7,3 +7,11 @@ import numpy as np
 
 # Initializing 1D array
 x = np.arange(10)
+print("Original array:")
+print(x)
+
+# Replace all odd numbers with -1 without affecting the original array
+y = np.where(x % 2 == 1, -1, x)
+print("\nModified array:")
+print(y)
+
