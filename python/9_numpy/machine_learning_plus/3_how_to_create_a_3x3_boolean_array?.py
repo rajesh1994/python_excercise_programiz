58ae882574a95361972763f871d10da39095713d
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 11:37:12 +0530

    Python excercise in numpy on 06-02-2019

--- python/9_numpy/machine_learning_plus/3_how_to_create_a_3x3_boolean_array?.py
+++ python/9_numpy/machine_learning_plus/3_how_to_create_a_3x3_boolean_array?.py
@@ -7,17 +7,17 @@ Problem Statement : 1. How to create a boolean array?
 import numpy as np
 
 # Method 1
-# Creating a boolean array bu using np.full() method
+# Creating a boolean array by using np.full() method
 bool_array1 = np.full((3, 3), True, dtype = bool)
 
 # Printing a (3 x 3) boolean array
-print("Printing a (3 x 3) boolean array")
-print(bool_array)
+print("Printing a 3x3 boolean array by using np.full() method:")
+print(bool_array1)
 
 # Method 2
-# Creating a boolean array bu using np.ones() method
+# Creating a boolean array by using np.ones() method
 bool_array2 = np.ones((3, 3), dtype = bool)
 
 # Printing a (3 x 3) boolean array
-print("Printing a (3 x 3) boolean array")
+print("\nPrinting a 3x3 boolean array by using np.ones() method:")
 print(bool_array2)
