4499cb119b393d5fe7b55bc4995d3782eea8f264
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 16:20:00 +0530

    Python excercise in numpy on 06-02-2019

---
+++ python/9_numpy/machine_learning_plus/11_how_to_get_the_common_items_between_two_python_numpy_arrays?.py
@@ -0,0 +1,18 @@
+"""
+Problem Statement : How to get the common items between two python numpy arrays?
+"""
+# Import numpy as 'np'
+import numpy as np
+
+# Initialize array 'x' & 'y'
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+y = np.array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+print("Array 'x' is:")
+print(x)
+print("Array 'y' is:")
+print(y)
+
+# Fetching common items between arrays 'x' & 'y'
+common_items = np.intersect1d(x, y)
+print("Common items between arrays 'x' & 'y' are:")
+print(common_items)
