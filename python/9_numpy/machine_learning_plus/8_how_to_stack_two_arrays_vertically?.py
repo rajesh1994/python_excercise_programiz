bbfa978cc53e0e6300ff0c3e8ab667eb563e8699
rajesh <rajeshsurya1994@gmail.com>
2019-02-06 14:02:20 +0530

    Python excercise in numpy on 06-02-2019

--- python/9_numpy/machine_learning_plus/8_how_to_stack_two_arrays_vertically?.py
+++ python/9_numpy/machine_learning_plus/8_how_to_stack_two_arrays_vertically?.py
@@ -12,3 +12,18 @@ print("Array 'x' is:")
 print(x)
 print("\nArray 'y' is:")
 print(y)
+
+# Method 1 using vstack() method
+vertical1  = np.vstack([x, y])
+print("Method 1 using vstack() method:")
+print(vertical1)
+
+# Method 2 using r_() method
+vertical2 = np.r_[x, y]
+print("Method 2 using r_() method:")
+print(vertical2)
+
+# Method 3 using concatenate() method
+vertical3 = np.concatenate([x, y])
+print("Method 1 using concatenate() method")
+print(vertical3)
