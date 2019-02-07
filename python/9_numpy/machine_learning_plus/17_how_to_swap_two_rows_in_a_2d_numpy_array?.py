e325a9ab66cf09cc3a57c11bab4fce3192af4293
rajesh <rajeshsurya1994@gmail.com>
2019-02-07 11:30:18 +0530

    Python excercise in numpy on 06-02-2019

---
+++ python/9_numpy/machine_learning_plus/17_how_to_swap_two_rows_in_a_2d_numpy_array?.py
@@ -0,0 +1,16 @@
+"""
+Problem Statement : How to swap two rows in a 2d numpy array?
+"""
+
+# Import numpy as 'np'
+import numpy as np
+
+# Initialize array 'x'
+x = np.arange(9).reshape(3, 3)
+print("Array 'x' is:")
+print(x)
+
+# Swap two rows in a 2D numpy array
+row_swap = x[[1, 0, 2], :]
+print("Swap two rows in a 2D numpy array:")
+print(row_swap)
