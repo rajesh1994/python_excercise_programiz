41fcdd32a53b348c4ef0c616b7cecd07a5a599bb
rajesh <rajeshsurya1994@gmail.com>
2019-02-11 16:36:41 +0530

    Python excercise in numpy on 11-02-2019

--- python/9_numpy/machine_learning_plus/27_how_to_convert_a_1d_array_of_tuples_to_a_2d_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/27_how_to_convert_a_1d_array_of_tuples_to_a_2d_numpy_array?.py
@@ -11,6 +11,11 @@ print("1D array is:")
 print(iris_1d)
 
 # Method 1: Convert each row to a list & get first 4 items
-iris_2d = np.array([row.tolist()[:4] for row in iris_1d])
-print("First 4 items from a 2D array:")
-print(iris_2d[:4])
+iris_2d_m1 = np.array([row.tolist()[:4] for row in iris_1d])
+print("Convert each row to a list & get first 4 items:")
+print(iris_2d_m1[:4])
+
+# Method 2: Import first 4 rows from source url
+iris_2d_m2 = np.genfromtxt(url, delimiter = ',', dtype = 'float', usecols = [0, 1, 2, 3])
+print("Import first 4 rows from source url:")
+print(iris_2d_m2[:4])
---
+++ python/9_numpy/machine_learning_plus/28_how_to_compute_the_mean,_median,_standard_deviation_of_a_numpy_array?.py
@@ -0,0 +1,6 @@
+"""
+Problem Statement : How to compute the mean, median, standard deviation of a numpy array?
+"""
+
+# Import numpy as 'np'
+import numpy as np
