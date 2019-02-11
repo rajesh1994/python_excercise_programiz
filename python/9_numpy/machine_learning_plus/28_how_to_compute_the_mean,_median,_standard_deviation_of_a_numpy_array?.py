06d2e55b1539255f6037131fa86d8b9a7d6eb912
rajesh <rajeshsurya1994@gmail.com>
2019-02-11 16:56:40 +0530

    Python excercise in numpy on 11-02-2019

--- python/9_numpy/machine_learning_plus/28_how_to_compute_the_mean,_median,_standard_deviation_of_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/28_how_to_compute_the_mean,_median,_standard_deviation_of_a_numpy_array?.py
@@ -4,3 +4,17 @@ Problem Statement : How to compute the mean, median, standard deviation of a num
 
 # Import numpy as 'np'
 import numpy as np
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter = ',', dtype = None)
+
+# Find the mean, median, standard deviation of iris's sepallength (1st column)
+
+sepallength = np.genfromtxt(url, delimiter = ',', dtype = 'float', usecols = [1])
+print("Sepal_Length column data:")
+print(sepallength)
+
+# Mean value of the column sepallength
+mean = np.mean(sepallength)
+print("\nMean value of the column sepallength:")
+print(mean)
