6fa83b8175cc55eccda45921b93a59e85463d8cb
rajesh <rajeshsurya1994@gmail.com>
2019-02-11 16:24:38 +0530

    Python excercise in numpy on 11-02-2019

--- python/9_numpy/machine_learning_plus/27_how_to_convert_a_1d_array_of_tuples_to_a_2d_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/27_how_to_convert_a_1d_array_of_tuples_to_a_2d_numpy_array?.py
@@ -7,8 +7,8 @@ import numpy as np
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 iris_1d = np.genfromtxt(url, delimiter = ',', dtype = None)
-#print("1D array is:")
-#print(iris_1d)
+print("1D array is:")
+print(iris_1d)
 
 # Method 1: Convert each row to a list & get first 4 items
 iris_2d = np.array([row.tolist()[:4] for row in iris_1d])
