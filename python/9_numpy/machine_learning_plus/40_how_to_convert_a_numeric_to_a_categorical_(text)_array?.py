40291cdb53da3d762a70094654809d8a88792c8d
rajesh <rajeshsurya1994@gmail.com>
2019-02-12 15:50:37 +0530

    Python excercise in numpy on 12-02-2019

--- python/9_numpy/machine_learning_plus/40_how_to_convert_a_numeric_to_a_categorical_(text)_array?.py
+++ python/9_numpy/machine_learning_plus/40_how_to_convert_a_numeric_to_a_categorical_(text)_array?.py
@@ -18,3 +18,9 @@ Bin the petal length (3rd) column of iris_2d to form a text array, such that if 
 
 # Bin petallength
 petal_length_bin = np.digitize(iris[:, 2].astype('float'), [0, 3, 5, 10])
+
+# Map it to a respective category
+lable_map = {1 : 'small', 2 : 'medium', 3 : 'large', 4 : np.nan}
+petal_length_cat = [lable_map[x] for x in petal_length_bin]
+print("Converting a numeric to a categorical (text) array:")
+print(petal_length_cat[:10])
