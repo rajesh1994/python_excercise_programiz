82c9ffad8458b03035f4b52e1e718a00d5b7922c
rajesh <rajeshsurya1994@gmail.com>
2019-02-12 15:59:16 +0530

    Python excercise in numpy on 12-02-2019

---
+++ python/9_numpy/machine_learning_plus/41_how_to_create_a_new_column_from_existing_columns_of_a_numpy_array?.py
@@ -0,0 +1,9 @@
+"""
+Problem Statement : How to create a new column from existing columns of a numpy array?
+"""
+
+# Import numpy as 'np'
+import numpy as np
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
