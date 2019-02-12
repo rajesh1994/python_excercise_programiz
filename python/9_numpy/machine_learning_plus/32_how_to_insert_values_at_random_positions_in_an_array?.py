df04d65b42cf3905082afe580099a611c1bda5ef
rajesh <rajeshsurya1994@gmail.com>
2019-02-12 11:41:27 +0530

    Python excercise in numpy on 12-02-2019

--- python/9_numpy/machine_learning_plus/32_how_to_insert_values_at_random_positions_in_an_array?.py
+++ python/9_numpy/machine_learning_plus/32_how_to_insert_values_at_random_positions_in_an_array?.py
@@ -10,8 +10,15 @@ iris_2d = np.genfromtxt(url, delimiter = ',', dtype = 'object')
 
 # Method 1:
 i, j = np.where(iris_2d)
-print(i, j)
+
 # i, j contain row numbers & column numbers of 600 elements of iris_2d
 np.random.seed(100)
 iris_2d[np.random.choice((i), 20), np.random.choice((j), 20)] = np.nan
-#print(iris_2d[: 10])
+print("Method 1:")
+print(iris_2d[: 10])
+
+# Method 2:
+np.random.seed(100)
+iris_2d[np.random.randint(150, size = 20), np.random.randint(4, size = 4)] = np.nan
+print("\nMethod 2:")
+print(iris_2d[: 10])
