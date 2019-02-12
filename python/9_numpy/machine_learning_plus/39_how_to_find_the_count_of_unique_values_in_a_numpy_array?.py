59f43e0faf3cdab49bb4190bc2e51497ddf3239c
rajesh <rajeshsurya1994@gmail.com>
2019-02-12 14:34:04 +0530

    Python excercise in numpy on 12-02-2019

--- python/9_numpy/machine_learning_plus/39_how_to_find_the_count_of_unique_values_in_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/39_how_to_find_the_count_of_unique_values_in_a_numpy_array?.py
@@ -6,3 +6,10 @@ Problem Statement : How to find the count of unique values in a numpy array?
 import numpy as np
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+iris = np.genfromtxt(url, delimiter = ',', dtype = 'object')
+
+# Find the unique values and the count of unique values in iris's species
+species = np.array([row.tolist()[4] for row in iris])
+unique = np.unique(species, return_counts = True)
+print("Unique values and the count of unique values in iris's species:")
+print(unique)
