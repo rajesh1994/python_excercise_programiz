86814be1f36f6ba708884cce0c6dbec0b76f7da2
rajesh <rajeshsurya1994@gmail.com>
2019-02-12 17:16:24 +0530

    Python excercise in numpy on 12-02-2019

--- python/9_numpy/machine_learning_plus/41_how_to_create_a_new_column_from_existing_columns_of_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/41_how_to_create_a_new_column_from_existing_columns_of_a_numpy_array?.py
@@ -9,10 +9,11 @@ url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
 # Create a new column for volume in iris_2d, where volume is (pi x petallength x sepal_length^2)/3
 iris_2d = np.genfromtxt(url, delimiter = ',', dtype = 'object')
-
+print("Original array:")
+print(iris_2d[:10])
 # Computing the voulume
 sepallength = iris_2d[:, 0].astype('float')
-petallemgth = iris_2d[:, 2].astype('float')
+petallength = iris_2d[:, 2].astype('float')
 volume = (np.pi * petallength * (sepallength ** 2)) // 3
 
 # Introduce new dimension to match iris_2d's
@@ -20,6 +21,6 @@ volume = volume[:, np.newaxis]
 
 # Add new column
 # New column from existing columns of a array
-out = np.hstack(iris_2d, volume)
-print("New column from existing columns of a array:")
-print(out)
+out = np.hstack([iris_2d, volume])
+print("\nNew column from existing columns of a array:")
+print(out[:10])
