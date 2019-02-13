7039913730db3707ea3dc218ad8883d38775fa49
rajesh <rajeshsurya1994@gmail.com>
2019-02-13 17:09:38 +0530

    Python excercise in numpy on 13-02-2019

--- python/9_numpy/machine_learning_plus/42_how_to_do_probabilistic_sampling_in_numpy?.py
+++ python/9_numpy/machine_learning_plus/42_how_to_do_probabilistic_sampling_in_numpy?.py
@@ -18,3 +18,10 @@ a = np.array(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
 species_out = np.random.choice(a, 150, p = [0.5, 0.25, 0.25])
 print("Probabilistic sampling:")
 print(species_out)
+
+# Approach 2: Probablistic Sampling (preferred)
+np.random.seed(100)
+probs = np.r_[np.linespace(0, 0.500, num = 50), np.linespace(0.501, 0.750, num = 50), np.linespace(0.751, 1, num = 50)]
+index = np.searchsorted(probs, np.random.random(150))
+species_out = species[index]
+print(np.unique(species_out, return_counts = True))
