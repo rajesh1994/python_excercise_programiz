b76bca5de33df32ec329d1e8e4e5f9817f888459
rajesh <rajeshsurya1994@gmail.com>
2019-02-13 14:59:15 +0530

    Python excercise in numpy on 13-02-2019

--- python/9_numpy/machine_learning_plus/36_how_to_find_the_correlation_between_two_columns_of_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/36_how_to_find_the_correlation_between_two_columns_of_a_numpy_array?.py
@@ -12,3 +12,13 @@ iris_2d = np.genfromtxt(url, delimiter = ',', dtype = 'float', usecols = [0, 1, 
 corrcoef = np.corrcoef(iris_2d[:, 0], iris_2d[:, 2])[0, 1]
 print("correlation between SepalLength(1st column) and column(PetalLength 3rd) in iris_2d:")
 print(corrcoef)
+
+"""
+Correlation Coefficient:
+1. Correlation coef indicates the degree of linear relationship between two numeric variables.
+2. It can range between -1 to +1.
+3. The p-value roughly indicates the probability of an uncorrelated system producing 
+4. datasets that have a correlation at least as extreme as the one computed.
+5. The lower the p-value (<0.01), stronger is the significance of the relationship.
+6. It is not an indicator of the strength.
+"""
--- python/9_numpy/machine_learning_plus/42_how_to_do_probabilistic_sampling_in_numpy?.py
+++ python/9_numpy/machine_learning_plus/42_how_to_do_probabilistic_sampling_in_numpy?.py
@@ -9,4 +9,12 @@ url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 iris = np.genfromtxt(url, delimiter = ',', dtype = 'object')
 
 # Randomly sample iris's species such that setose is twice the number of versicolor and virginica
+# Get the species column
+species = iris[:,4]
 
+# Approach 1: Generate Probablistically
+np.random.seed(100)
+a = np.array(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
+species_out = np.random.choice(a, 150, p = [0.5, 0.25, 0.25])
+print("Probabilistic sampling:")
+print(species_out)
