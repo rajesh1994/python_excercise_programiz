1e81c0999b789f963421818b9acc8673c2282f32
rajesh <rajeshsurya1994@gmail.com>
2019-02-14 15:26:01 +0530

    Python excercise in numpy on 14-02-2019

--- python/9_numpy/machine_learning_plus/47_how_to_replace_all_values_greater_than_a_given_value_to_a_given_cutoff?.py
+++ python/9_numpy/machine_learning_plus/47_how_to_replace_all_values_greater_than_a_given_value_to_a_given_cutoff?.py
@@ -8,4 +8,13 @@ import numpy as np
 np.set_printoptions(precision = 2)
 np.random.seed(100)
 array = np.random.uniform(10, 50, 20)
-print(array)
+#print(np.sort(array))
+
+# From the array, replace all values greater than 30 to 30 and less than 10 to 10
+# Method 1: By using np.clip()
+print("Method 1: By using np.clip():")
+print(np.sort(np.clip(array, a_min = 10, a_max = 30)))
+
+# Method 2: By using np.where()
+print("\nMethod 2: By using np.where():")
+print(np.sort(np.where(array < 10, 10, np.where(array > 30, 30, array))))
