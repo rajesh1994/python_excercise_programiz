820764eaea64d50538f821f7ce0c6d6e3416108f
rajesh <rajeshsurya1994@gmail.com>
2019-02-14 15:00:57 +0530

    Python excercise in numpy on 14-02-2019

---
+++ python/9_numpy/machine_learning_plus/47_how_to_replace_all_values_greater_than_a_given_value_to_a_given_cutoff?.py
@@ -0,0 +1,11 @@
+"""
+Problem Statement : How to replace all values greater than a given value to a given cutoff?
+"""
+
+# Import numpy as 'np'
+import numpy as np
+
+np.set_printoptions(precision = 2)
+np.random.seed(100)
+array = np.random.uniform(10, 50, 20)
+print(array)
