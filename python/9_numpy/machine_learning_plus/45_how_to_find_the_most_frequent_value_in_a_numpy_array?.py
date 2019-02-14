5cf4c4e4896ea842fb82c4305ffa3830d23d0223
rajesh <rajeshsurya1994@gmail.com>
2019-02-14 14:01:38 +0530

    Python excercise in numpy on 14-02-2019

--- python/9_numpy/machine_learning_plus/45_how_to_find_the_most_frequent_value_in_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/45_how_to_find_the_most_frequent_value_in_a_numpy_array?.py
@@ -6,7 +6,10 @@ Problem Statement : How to find the most frequent value in a numpy array?
 import numpy as np
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-iris = np.genfromtxt(url, delimiter = ',', dtype ='object')
+iris = np.genfromtxt(url, delimiter = ',', dtype ='float')
 
 # Find the most frequent value of petal length (3rd column) in iris dataset
-vals, counts = np.unique(iris[:, 2])
+vals, counts = np.unique(iris[:, 2], return_counts = "True")
+print(vals, counts)
+print("Most frequent value of petal length column:")
+print(vals[np.argmax(counts)])
