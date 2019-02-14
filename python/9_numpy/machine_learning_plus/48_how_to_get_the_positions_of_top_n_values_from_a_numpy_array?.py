defc783f206c968754cfa3b1ff38ca9d8429061d
rajesh <rajeshsurya1994@gmail.com>
2019-02-14 17:36:36 +0530

    Python excercise in numpy on 14-02-2019

--- python/9_numpy/machine_learning_plus/48_how_to_get_the_positions_of_top_n_values_from_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/48_how_to_get_the_positions_of_top_n_values_from_a_numpy_array?.py
@@ -8,16 +8,30 @@ import numpy as np
 np.set_printoptions(precision = 2)
 np.random.seed(100)
 a = np.random.uniform(1, 50, 20)
-#print(array)
+print("Array 'a' is:")
+print(np.sort(a))
 
 # Get the positions of top 5 maximum values in a given array a.
 
 # Solution 1:
-print("Solution 1:")
-print(a.argsort())
-
-# Solution 2:
-print("\nSolution 2:")
+print("\nSolution 1:")
 print(np.argpartition(-a, 5)[:5])
 
-# 
+# Below methods will get you the values
+
+# Method 1:
+print("\nMethod 1:")
+print(a[a.argsort()][-5:])
+
+# Method 2:
+print("\nMethod 2:")
+print(np.sort(a)[-5:])
+
+# Method 3:
+print("\nMethod 3:")
+print(np.partition(a, kth = -5)[-5:])
+#np.partition(a, kth=-5)[-5:]
+
+# Method 4:
+print("\nMethod 4:")
+print(np.argpartition(-a, 5)[:5])
