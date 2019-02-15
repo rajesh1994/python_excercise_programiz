c555dfcbaca12fc7e452c4a049caf993a36ad5f7
rajesh <rajeshsurya1994@gmail.com>
2019-02-15 12:56:37 +0530

    Python excercise in numpy on 15-02-2019

--- python/9_numpy/machine_learning_plus/50_how_to_convert_an_array_of_arrays_into_a_flat_1d_array?.py
+++ python/9_numpy/machine_learning_plus/50_how_to_convert_an_array_of_arrays_into_a_flat_1d_array?.py
@@ -5,3 +5,12 @@ Problem Statement : How to convert an array of arrays into a flat 1d array?
 # Import numpy as 'np'
 import numpy as np
 
+a1 = np.arange(3)
+a2 = np.arange(3, 7)
+a3 = np.arange(7, 10)
+arrays_of_array = np.array([a1, a2, a3])
+print("Arrays of array:")
+print(arrays_of_array)
+
+# Solution 1: By using np.concatenate()
+array_2d = np.concatenate(arrays_of_array)
