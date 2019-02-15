6ac38efcbc532b9d03dccd6039d40ef99e383bc3
rajesh <rajeshsurya1994@gmail.com>
2019-02-15 17:03:52 +0530

    Python excercise in numpy on 15-02-2019

--- python/9_numpy/machine_learning_plus/52_how_to_create_row_numbers_grouped_by_a_categorical_variable?.py
+++ python/9_numpy/machine_learning_plus/52_how_to_create_row_numbers_grouped_by_a_categorical_variable?.py
@@ -12,3 +12,6 @@ species = np.genfromtxt(url, delimiter = ',', dtype = 'str', usecols = [4])
 species_small = np.sort(np.random.choice(species, size = 20))
 print("Array 'Species Small':")
 print(species_small)
+
+# Create row numbers grouped by a categorical variable. Use the following sample from iris species as input.
+print([i for val in np.unique(species_small) for i, grp in enumerate(species_small[species_small == val])])
