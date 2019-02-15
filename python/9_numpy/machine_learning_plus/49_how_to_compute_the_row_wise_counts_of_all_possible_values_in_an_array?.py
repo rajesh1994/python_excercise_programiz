6c3ba3f7bbae73c149d551c7c8d104185314d6dc
rajesh <rajeshsurya1994@gmail.com>
2019-02-15 12:41:40 +0530

    Python excercise in numpy on 15-02-2019

--- python/9_numpy/machine_learning_plus/49_how_to_compute_the_row_wise_counts_of_all_possible_values_in_an_array?.py
+++ python/9_numpy/machine_learning_plus/49_how_to_compute_the_row_wise_counts_of_all_possible_values_in_an_array?.py
@@ -5,3 +5,17 @@ Problem Statement : How to compute the row wise counts of all possible values in
 # Import numpy as 'np'
 import numpy as np
 
+np.random.seed(100)
+a = np.random.randint(1, 10, size = (5, 10))
+print("Array 'a' is:")
+print(a)
+
+# Compute the counts of unique values row-wise
+def counts_of_all_values_rowwise(array2d):
+    # Unique values & its counts row-wise
+    num_counts_array = [np.unique(row, return_counts = True) for row in array2d]
+    
+    # Counts of all values row-wise
+    return([[int(b[a == i])] if i in a else 0 for i in np.unique(array2d) for a, b in num_counts_array])
+print("\nCompute the counts of unique values row-wise:")
+print([counts_of_all_values_rowwise(a)])
