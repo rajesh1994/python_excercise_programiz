dd1d73d8ceb66560afdbf21f2dc16c0ed8fe0197
rajesh <rajeshsurya1994@gmail.com>
2019-02-18 17:46:15 +0530

    Python excercise in numpy on 18-02-2019

--- python/9_numpy/machine_learning_plus/55_how_to_rank_items_in_a_multidimensional_array_using_numpy?.py
+++ python/9_numpy/machine_learning_plus/55_how_to_rank_items_in_a_multidimensional_array_using_numpy?.py
@@ -5,3 +5,9 @@ Problem Statement : How to rank items in a multidimensional array using numpy?
 # Import numpy as 'np'
 import numpy as np
 
+np.random.seed(10)
+a = np.random.randint(20, size = [2, 5])
+print("Array 'a' is:")
+print(a)
+print("\nRank items in a multidimensional array:")
+print(a.ravel().argsort().argsort().reshape(a.shape))
