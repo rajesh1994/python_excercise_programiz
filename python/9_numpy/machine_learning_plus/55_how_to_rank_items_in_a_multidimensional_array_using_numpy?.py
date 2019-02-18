bcb5889296bd2fb3aac8fd428df32c452e020287
rajesh <rajeshsurya1994@gmail.com>
2019-02-18 18:03:00 +0530

    Python excercise in numpy on 18-02-2019

--- python/9_numpy/machine_learning_plus/55_how_to_rank_items_in_a_multidimensional_array_using_numpy?.py
+++ python/9_numpy/machine_learning_plus/55_how_to_rank_items_in_a_multidimensional_array_using_numpy?.py
@@ -5,6 +5,7 @@ Problem Statement : How to rank items in a multidimensional array using numpy?
 # Import numpy as 'np'
 import numpy as np
 
+# Create a rank array of the same shape as a given numeric array a.
 np.random.seed(10)
 a = np.random.randint(20, size = [2, 5])
 print("Array 'a' is:")
--- python/9_numpy/machine_learning_plus/56_how_to_find_the_maximum_value_in_each_row_of_a_numpy_array_2d?.py
+++ python/9_numpy/machine_learning_plus/56_how_to_find_the_maximum_value_in_each_row_of_a_numpy_array_2d?.py
@@ -5,3 +5,16 @@ Problem Statement : How to find the maximum value in each row of a numpy array 2
 # Import numpy as 'np'
 import numpy as np
 
+# Compute the maximum for each row in the given array.
+np.random.seed(100)
+a = np.random.randint(1, 10, [5, 3])
+print("Array 'a' is:")
+print(a)
+
+# Method 1: By using np.amax() method
+print("\nMethod 1:\nMaximum value in each row of an array:")
+print(np.amax(a, axis = 1))
+
+# Method 2: By using np.apply_along_axis() method
+print("\nMethod 2:\nMaximum value in each of an array:")
+print(np.apply_along_axis(np.max, arr = a, axis = 1))
