d315f4fa529248a86ed454670fa2e5a5c2d92004
rajesh <rajeshsurya1994@gmail.com>
2019-02-18 14:39:22 +0530

    Python excercise in numpy on 18-02-2019

--- python/9_numpy/machine_learning_plus/53_how_to_create_groud_ids_based_on_a_given_categorical_variable?.py
+++ python/9_numpy/machine_learning_plus/53_how_to_create_groud_ids_based_on_a_given_categorical_variable?.py
@@ -6,11 +6,11 @@ Problem Statement : How to create groud ids based on a given categorical variabl
 import numpy as np
 
 url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
-iris = np.genfromtxt(url, delimiter = ',', dtype = 'float')
+iris = np.genfromtxt(url, delimiter = ',', dtype = 'object')
 
 # What is the value of second longest petallength of species setosa
 # Get the species & petal length column
-petal_length_setosa = iris[iris[:, 4] == b'Iris-setosa', [2]]
+petal_length_setosa = iris[iris[:, 4] == b'Iris-setosa', [2]].astype('float')
 
 # Get the second last value
 print("Second longest petallength of species setosa:")
