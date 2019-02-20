dbe11d860ee71b26e7e138a38d68664cfde2c5a6
rajesh <rajeshsurya1994@gmail.com>
2019-02-20 15:08:41 +0530

    Python excercise in numpy on 20-02-2019

--- python/9_numpy/machine_learning_plus/57_how_to_compute_the_min-by-max_for_each_row_for_a_numpy_array_2d?.py
+++ python/9_numpy/machine_learning_plus/57_how_to_compute_the_min-by-max_for_each_row_for_a_numpy_array_2d?.py
@@ -4,3 +4,11 @@ Problem Statement : How to compute the min-by-max for each row for a numpy array
 
 # Import numpy as 'np'
 import numpy as np
+
+np.random.seed(100)
+a = np.random.randint(1, 10, [5, 3])
+print("Array 'a' is:")
+print(a)
+# Compute the min-by-max for each row for given 2d numpy array.
+print("\nMin-by-max for each row for a numpy array 2d:")
+print(np.apply_along_axis(lambda x: np.min(x)/np.max(x), arr = a, axis = 1))
