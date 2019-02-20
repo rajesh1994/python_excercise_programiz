8df3228304c3f3626f2b14f996f0676c159ce4f2
rajesh <rajeshsurya1994@gmail.com>
2019-02-20 17:02:09 +0530

    Python excercise in numpy on 20-02-2019

--- python/9_numpy/machine_learning_plus/58_how_to_find_the_duplicate_records_in_a_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/58_how_to_find_the_duplicate_records_in_a_numpy_array?.py
@@ -4,3 +4,22 @@ Problem Statement : How to find the duplicate records in a numpy array?
 
 # Import numpy as 'np'
 import numpy as np
+
+np.random.seed(100)
+a  = np.random.randint(1, 5, 10)
+print("Array 's' is:")
+print(a)
+
+## Solution
+# There is no direct function to do this as of 1.13.3
+
+# Create an all True array
+out = np.full(a.shape[0], True)
+
+# Find the index position of unique elements
+unique_positions = np.unique(a, return_index = True)[1]
+
+# Mark those positions as False
+out[unique_positions] = False
+
+print(out)
