7dbd91e5c45d90ebb04d2f96eea068b6bea747b6
rajesh <rajeshsurya1994@gmail.com>
2019-02-20 17:40:59 +0530

    Python excercise in numpy on 20-02-2019

--- python/9_numpy/machine_learning_plus/60_how_to_convert_a_pil_image_to_numpy_array?.py
+++ python/9_numpy/machine_learning_plus/60_how_to_convert_a_pil_image_to_numpy_array?.py
@@ -4,3 +4,21 @@ Problem Statement : How to convert a PIL image to numpy array?
 
 # Import numpy as 'np'
 import numpy as np
+from io import BytesIO
+from PIL import Image
+import PIL, requests
+
+# Import image from URL
+URL = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Denali_Mt_McKinley.jpg'
+response = request.get(URL)
+
+# Read it as Image
+i = Image.open(BytesIO(response.content))
+
+# Optinaly resize
+i = i.resize([150, 150])
+
+# Convert to numpy array
+array = np.asarray(i)
+print("PIL image to array:")
+print(i)
