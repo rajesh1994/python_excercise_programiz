b2e5fe4e9442bceee2f46c44250b13b6b8c26aa9
rajesh <rajeshsurya1994@gmail.com>
2019-02-21 17:26:30 +0530

    Python excercise in numpy on 21-02-2019

--- python/9_numpy/machine_learning_plus/64_how_to_subtract_a_1d_array_from_a_2d_array,_where_each_item_of_1d_array_subtracts_from_respective_row?.py
+++ python/9_numpy/machine_learning_plus/64_how_to_subtract_a_1d_array_from_a_2d_array,_where_each_item_of_1d_array_subtracts_from_respective_row?.py
@@ -5,4 +5,13 @@ Problem Statement : How to subtract a 1d array from a 2d array, where each item 
 # Import numpy as 'np'
 import numpy as np
 
-
+# Initialize 1d & 2d array:
+a = np.array([1, 2, 3, 4])
+b = np.array([[3, 3, 3], [4, 4, 4], [5, 5, 5], [6, 6, 6]])
+print("Array 'a':")
+print(a)
+print("\nArray 'b':")
+print(b)
+# Subtract the 1d array a_1d from the 2d array a_2d, such that each item of a_1d subtracts from respective row of a_2d.
+print("\nSubtract the 1d array from the 2d array:")
+print(b - a[:,None])
