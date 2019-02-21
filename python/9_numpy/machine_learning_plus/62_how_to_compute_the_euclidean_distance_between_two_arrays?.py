ba446b7791034a18f510ecf0921a27845e5c87df
rajesh <rajeshsurya1994@gmail.com>
2019-02-21 16:19:08 +0530

    Python excercise in numpy on 21-02-2019

--- python/9_numpy/machine_learning_plus/62_how_to_compute_the_euclidean_distance_between_two_arrays?.py
+++ python/9_numpy/machine_learning_plus/62_how_to_compute_the_euclidean_distance_between_two_arrays?.py
@@ -5,4 +5,14 @@ Problem Statement : How to compute the euclidean distance between two arrays?
 # Import numpy as 'np'
 import numpy as np
 
-
+# Initialize array 'a' & 'b'
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([6, 7, 8, 9, 10])
+print("Array 'a':")
+print(a)
+print("\nArray 'b':")
+print(b)
+# Compute the euclidean distance between two arrays a and b.
+euc_dist = np.linalg.norm(a-b)
+print("\nEuclidean distance between two arrays a and b:")
+print(euc_dist)
--- python/9_numpy/machine_learning_plus/63_how_to_find_all_the_local_maxima_(or_peaks)_in_a_1d_array?.py
+++ python/9_numpy/machine_learning_plus/63_how_to_find_all_the_local_maxima_(or_peaks)_in_a_1d_array?.py
@@ -5,4 +5,3 @@ Problem Statement : How to find all the local maxima (or peaks) in a 1d array?
 # Import numpy as 'np'
 import numpy as np
 
-
