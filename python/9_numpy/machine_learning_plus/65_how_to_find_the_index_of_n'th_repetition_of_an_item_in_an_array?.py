f8a11b8e1ff9fbeabf3aa8a1b0d56ee2d8cc2681
rajesh <rajeshsurya1994@gmail.com>
2019-02-21 17:58:36 +0530

    Python excercise in numpy on 21-02-2019

--- python/9_numpy/machine_learning_plus/65_how_to_find_the_index_of_n'th_repetition_of_an_item_in_an_array?.py
+++ python/9_numpy/machine_learning_plus/65_how_to_find_the_index_of_n'th_repetition_of_an_item_in_an_array?.py
@@ -5,4 +5,17 @@ Problem Statement : How to find the index of n'th repetition of an item in an ar
 # Import numpy as 'np'
 import numpy as np
 
+# Initialize array 'x'
+x = np.array([1, 2, 1, 1, 3, 4, 3, 1, 1, 2, 1, 1, 2])
+n = 5
+# Find the index of 5th repetition of number 1 in x.
 
+# Solution 1: By using list comprehension
+repet1 = [i for i, v in enumerate(x) if v == 1][n - 1]
+print("\nSolution 1: By using list comprehension:")
+print(repet1)
+
+# Solution 2: By using numpy version
+repet2 = np.where(x == 1)[0][n - 1]
+print("\nSolution 2: By using numpy version:")
+print(repet2)
