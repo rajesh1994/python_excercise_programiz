650fb45ed8c63615096052a811de350bc88087f8
rajesh <rajeshsurya1994@gmail.com>
2019-02-21 18:26:22 +0530

    Python excercise in numpy on 21-02-2019

--- python/9_numpy/machine_learning_plus/66_how_to_convert_numpy's_datetime64_object_to_datetime's_datetime_object?.py
+++ python/9_numpy/machine_learning_plus/66_how_to_convert_numpy's_datetime64_object_to_datetime's_datetime_object?.py
@@ -5,4 +5,18 @@ Problem Statement : How to convert numpy's datetime64 object to datetime's datet
 # Import numpy as 'np'
 import numpy as np
 
+# Input : A numpy datetime64 object
+dt64 = np.datetime64('2018-02-25 22:10:10')
 
+# Convert numpy's datetime64 object to datetime's datetime object
+
+# Solution 1: By using tolist() method
+from datetime import datetime
+date1 = dt64.tolist()
+print("Solution 1: By using tolist() method:")
+print(date1)
+
+# Solution 2: By using astype() method
+date2 = dt64.astype(datetime)
+print("\nSolution 2: By using date2() astype:")
+print(date2)
