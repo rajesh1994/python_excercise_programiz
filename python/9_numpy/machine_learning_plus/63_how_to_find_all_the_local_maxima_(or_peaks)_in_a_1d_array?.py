459ab65f670f111860e16f12df106e6055588f7c
rajesh <rajeshsurya1994@gmail.com>
2019-02-21 16:19:31 +0530

    Python excercise in numpy on 21-02-2019

--- python/9_numpy/machine_learning_plus/63_how_to_find_all_the_local_maxima_(or_peaks)_in_a_1d_array?.py
+++ python/9_numpy/machine_learning_plus/63_how_to_find_all_the_local_maxima_(or_peaks)_in_a_1d_array?.py
@@ -5,3 +5,13 @@ Problem Statement : How to find all the local maxima (or peaks) in a 1d array?
 # Import numpy as 'np'
 import numpy as np
 
+# Initialize array 'a':
+a = np.array([8, 10, 1, 4, 0, 6, 2, 7, 11, 101, 3])
+print("Array 'a' is:")
+print(a)
+
+# Find all the peaks in a 1D numpy array a. Peaks are points surrounded by smaller values on both sides.
+double_diff = np.diff(np.sign(np.diff(a)))
+peak_location = np.where(double_diff == -2)[0] + 1
+print("\nLocal maxima in array 'a':")
+print(peak_location)
