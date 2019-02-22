cfa9e415b4d97bb599a4d36d86113627fefb2b07
rajesh <rajeshsurya1994@gmail.com>
2019-02-22 17:41:36 +0530

    Python excercise in numpy on 22-02-2019

--- python/9_numpy/machine_learning_plus/70_how_to_create_strides_from_a_given_1d_array?.py
+++ python/9_numpy/machine_learning_plus/70_how_to_create_strides_from_a_given_1d_array?.py
@@ -5,4 +5,13 @@ Problem Statement : How to create strides from a given 1D array?
 # Import numpy as 'np'
 import numpy as np
 
+# Initialize an array 'x'
+x = np.arange(15)
+print("Array 'x' is:")
+print(x)
 
+def gen_strides(x, stride_len = 5, window_len = 5):
+    n_strides = ((x.size-window_len)//stride_len) + 1
+    return np.array([x[s:(s+window_len)] for s in np.arange(0, n_strides * stride_len, stride_len)])
+print("\n2d array from 1d array using strides:")
+print(gen_strides(x, stride_len = 2, window_len = 4))
