c802853f5defc43ecc54874cf917b1be527b7e19
rajesh <rajeshsurya1994@gmail.com>
2019-02-22 15:04:17 +0530

    Python excercise in numpy on 22-02-2019

--- python/9_numpy/machine_learning_plus/68_how_to_create_a_numpy_array_sequence_given_only_the_starting_point,_length_and_the_step?.py
+++ python/9_numpy/machine_learning_plus/68_how_to_create_a_numpy_array_sequence_given_only_the_starting_point,_length_and_the_step?.py
@@ -5,4 +5,13 @@ Problem Statement : How to create a numpy array sequence given only the starting
 # Import numpy as 'np'
 import numpy as np
 
+# Create a numpy array of length 10, starting from 5 and has a step of 3 between consecutive numbers.
+length = int(input("Enter the length of the numpy array :"))
+start = int(input("\nEnter the starting poition of numpy array :"))
+step = int(input("\nEnter the step value :"))
 
+def seq(start, length, step):
+    end = start + (length * step)
+    return np.arange(start, end, step)
+print("\nNumpy array sequence:")
+print(seq(start, length, step))
