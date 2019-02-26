88e0e33d73199c2094e47f1358df82ceb85e377b
rajesh <rajeshsurya1994@gmail.com>
2019-02-26 16:04:01 +0530

    Python excercise in pandas on 26-02-2019

--- python/10_pandas/data_camp/2.3_different_types_of_indexing_in_a_dataframe.py
+++ python/10_pandas/data_camp/2.3_different_types_of_indexing_in_a_dataframe.py
@@ -12,4 +12,24 @@ df = pd.DataFrame(data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], index = [2, 'A', 4],
 print("DataFrame 'df' is:")
 print(df)
 
+# Pass '2' to 'loc'
+print("\nPicking elements based on DataFrame have an index labled 2:")
+print(df.loc[2])
 
+# Pass '2' to 'iloc'
+print("\nPicking elements based on DataFrame that are at index 2:")
+print(df.iloc[2])
+
+# Pass '2' to 'ix'
+print("\nPicking elements if your index is not solely integer-based:")
+print(df.ix[2])
+
+"""
+Concept of loc and how it differs from other indexing attributes such as .iloc[] and .ix[]:
+
+	.loc[] works on labels of your index. This means that if you give in loc[2], you look for the values of your DataFrame that have an index labeled 2.
+
+	.iloc[] works on the positions in your index. This means that if you give in iloc[2], you look for the values of your DataFrame that are at index ’2`.
+
+	.ix[] is a more complex case: when the index is integer-based, you pass a label to .ix[]. ix[2] then means that you’re looking in your DataFrame for values that have an index labeled 2. This is just like .loc[]! However, if your index is not solely integer-based, ix will work with positions, just like .iloc[].
+"""
