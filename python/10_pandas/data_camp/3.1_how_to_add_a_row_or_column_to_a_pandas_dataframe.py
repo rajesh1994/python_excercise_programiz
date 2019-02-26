3eed1fa64c3a1a0c6bd43da2310f2a6e1a2c0beb
rajesh <rajeshsurya1994@gmail.com>
2019-02-26 14:17:41 +0530

    Python excercise in pandas on 26-02-2019

---
+++ python/10_pandas/data_camp/3.1_how_to_add_a_row_or_column_to_a_pandas_dataframe.py
@@ -0,0 +1,3 @@
+"""
+Problem Statement : 3.1 How To Add a Row or Column to a Pandas DataFrame
+"""
--- python/10_pandas/data_camp/3_how_to_add_an_index,_row_or_column_to_a_pandas_dataframe.py
+++ python/10_pandas/data_camp/3_how_to_add_an_index,_row_or_column_to_a_pandas_dataframe.py
@@ -1,3 +1,17 @@
 """
-Problem Statement : 3. How To Add an Index, Row or Column to a Pandas DataFrame
+Problem Statement : 3.0 How To Add an Index to a Pandas DataFrame
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns = ['A', 'B', 'C'])
+print("DataFrame 'np' is:")
+print(df)
+
+# Setting 'C' as the index to DataFrame 'df'
+print("\nSetting 'C' as the index to DataFrame 'df':")
+print(df.set_index('C'))
