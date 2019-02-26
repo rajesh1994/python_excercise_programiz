76785eb6785236318c4a14173ce4abf951e48580
rajesh <rajeshsurya1994@gmail.com>
2019-02-26 11:33:03 +0530

    Python excercise in pandas on 26-02-2019

--- python/10_pandas/data_camp/1.1_different_forms_of_pandas_dataframe.py
+++ python/10_pandas/data_camp/1.1_different_forms_of_pandas_dataframe.py
@@ -7,4 +7,8 @@ Problem Statement : 1.1 Different Forms of Pandas DataFrame
 import numpy as np
 import pandas as pd
 
+# Take 2d array as input to your DataFrame
+my_2darray = np.array([[1, 2, 3], [4, 5, 6]])
 
+# Printing my_2darray array as a form of DataFrame
+print(pd.DataFrame(data = my_2darray[0:, 0:]))
