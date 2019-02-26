52a4e4c119cdcb3d9d80fcc156767b60107bac19
rajesh <rajeshsurya1994@gmail.com>
2019-02-26 16:26:46 +0530

    Python excercise in pandas on 26-02-2019

--- python/10_pandas/data_camp/3.1_how_to_add_a_row_or_column_to_a_pandas_dataframe.py
+++ python/10_pandas/data_camp/3.1_how_to_add_a_row_or_column_to_a_pandas_dataframe.py
@@ -11,3 +11,9 @@ import pandas as pd
 df = pd.DataFrame(data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns = ['A', 'B', 'C'])
 print("DataFrame 'df' is:")
 print(df)
+
+# Adding a column to a DataFrame
+df['D'] = df.index
+
+print("After adding column 'D' to Data")
+print(df)
