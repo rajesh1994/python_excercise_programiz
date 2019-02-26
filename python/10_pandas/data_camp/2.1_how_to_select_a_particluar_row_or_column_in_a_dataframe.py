bca26d39c50c16f5f959f7f743874aa49ed8e810
rajesh <rajeshsurya1994@gmail.com>
2019-02-26 13:48:43 +0530

    Python excercise in pandas on 26-02-2019

--- python/10_pandas/data_camp/2.1_how_to_select_a_particluar_row_or_column_in_a_dataframe.py
+++ python/10_pandas/data_camp/2.1_how_to_select_a_particluar_row_or_column_in_a_dataframe.py
@@ -2,4 +2,20 @@
 Problem Statement : 2.1 How to select a particluar row/column in a dataframe
 """
 
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
 
+# Initialize DataFrame 'df'
+df = pd.DataFrame(data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns = ['A', 'B', 'C'])
+print("DataFrame 'df' is:")
+print(df)
+
+# Method 1 : Using 'iloc[]' to select row '0'
+print("\nMethod 1 : Using 'iloc[]' to select row '0':")
+print(df.iloc[0])
+
+# Method 2 : Using 'loc[]' to select column 'A'
+print("\nMethod 2 : Using 'loc[]' to select column 'A':")
+print(df.loc[:, 'A'])
