35fb7f21baf91fca8058856ec22b7ef0c53114b3
rajesh <rajeshsurya1994@gmail.com>
2019-02-27 18:02:59 +0530

    Python excercise in pandas on 27-02-2019

--- python/10_pandas/data_camp/5_how_to_rename_the_index_or_columns_of_a_pandas_dataframe.py
+++ python/10_pandas/data_camp/5_how_to_rename_the_index_or_columns_of_a_pandas_dataframe.py
@@ -14,6 +14,14 @@ print(df)
 
 # Difining new names of columns 'A', 'B' & 'C'
 new_cols = {'A' : 'a', 'B' : 'b', 'C' : 'c'}
-
-# Renaming columns 'A', 'B' & 'C'
+# Renaming columns 'A', 'B' & 'C' by using rename() method
 df.rename(columns = new_cols, inplace = True)
+print("\nRenaming columns 'A', 'B' & 'C' to 'a', 'b' & 'c':")
+print(df)
+
+# Difining new names of rows 1, 2 & 3
+new_rows = {1 : 'one', 2 : 'two', 3 : 'three'}
+# Renaming rows 1, 2, 3 by using rename() method
+df.rename(index = new_rows, inplace = True)
+print("\nRenaming rows 1, 2 & 3 to 'one', 'two' & 'three':")
+print(df)
