c70f168bac296424aeacde73b3113618d081e490
rajesh <rajeshsurya1994@gmail.com>
2019-02-27 15:19:17 +0530

    Python excercise in pandas on 27-02-2019

--- python/10_pandas/data_camp/4.1_how_to_delete_rows_or_columns_from_a_pandas_data_frame.py
+++ python/10_pandas/data_camp/4.1_how_to_delete_rows_or_columns_from_a_pandas_data_frame.py
@@ -1,3 +1,24 @@
 """
 Problem Statement : 4.1 How to Delete Rows or Columns From a Pandas Data Frame
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [40, 50, 60], [23, 35, 57]]), index = [1.2, 2.4, 4.6, 9.3, 5.3], columns = ['A', 'B', 'C'])
+print("DataFrame 'df' is:")
+print(df)
+
+# Deleting a column from DataFrame
+# Drop the column with label 'A'
+df.drop('A', axis = 1, inplace= True)
+print("\nDrop the column with label 'A:")
+print(df)
+
+# Drop the column at Position 1
+df.drop(df.columns[[1]], axis = 1)
+print("\nDrop the column at Position 1:")
+print(df)
