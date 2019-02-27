ffd0c872e9126a9b85a9026feb2f69a25daa77a8
rajesh <rajeshsurya1994@gmail.com>
2019-02-27 17:15:03 +0530

    Python excercise in pandas on 27-02-2019

--- python/10_pandas/data_camp/5_how_to_rename_the_index_or_columns_of_a_pandas_dataframe.py
+++ python/10_pandas/data_camp/5_how_to_rename_the_index_or_columns_of_a_pandas_dataframe.py
@@ -1,3 +1,19 @@
 """
 Problem Statement : 5. How to Rename the Index or Columns of a Pandas DataFrame
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), index = [1, 2, 3], columns = ['A', 'B', 'C'])
+print("DataFrame 'df' is:")
+print(df)
+
+# Difining new names of columns 'A', 'B' & 'C'
+new_cols = {'A' : 'a', 'B' : 'b', 'C' : 'c'}
+
+# Renaming columns 'A', 'B' & 'C'
+df.rename(columns = new_cols, inplace = True)
