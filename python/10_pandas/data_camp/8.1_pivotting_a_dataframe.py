a5993eb1adeefb27f6051b0ef23b599479c73b92
rajesh <rajeshsurya1994@gmail.com>
2019-02-28 16:29:06 +0530

    Python excercise in pandas on 28-02-2019

---
+++ python/10_pandas/data_camp/8.1_pivotting_a_dataframe.py
@@ -0,0 +1,17 @@
+"""
+Problem Statement : 8.1 Pivotting a DataFrame
+"""
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame({'category' : ['Cleaning', 'Cleaning', 'Entertainment', 'Entertainment', 'Tech', 'Tech'], 'store' : ['Walmart', 'Dia', 'Walmart', 'Fnac', 'Dia', 'Walmart'], 'price' : [11.42, 23.50, 19.99, 15.95, 55.75, 111.55], 'testscore' : [4, 3, 5, 7, 5, 8]})
+print("DataFrame 'df' is:")
+print(df)
+
+pivot_df = df.pivot(index = 'category', columns = 'store')
+print("Pivotting a DataFrame:")
+print(pivot_df)
