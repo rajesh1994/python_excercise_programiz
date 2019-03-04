e35da7a3eadc43f921291bca5e060e19b25cd31f
rajesh <rajeshsurya1994@gmail.com>
2019-03-04 16:19:10 +0530

    Python excercise in pandas on 04-03-2019

--- python/10_pandas/machinelearningplus/1_how_to_import_pandas_and_check_the_version?.py
+++ python/10_pandas/machinelearningplus/1_how_to_import_pandas_and_check_the_version?.py
@@ -1,3 +1,10 @@
 """
 Problem Statement : 1. How to import pandas and check the version?
 """
+# Import numpy as np
+import pandas as pd
+
+# Printing the version of pandas library
+print("Printing the version of pandas library:")
+print(pd.__version__)
+print(pd.show_versions(as_json = True))
--- python/10_pandas/machinelearningplus/2_how_to_create_a_series_from_a_list,_numpy_array_and_dict?.py
+++ python/10_pandas/machinelearningplus/2_how_to_create_a_series_from_a_list,_numpy_array_and_dict?.py
@@ -1,3 +1,16 @@
 """
 Problem Statement : 2. How to create a series from a list, numpy array and dict?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize list, numpy array & dict
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarray = np.arange(26)
+mydict = dict(zip(mylist, myarray))
+
+# Creating a series from a list, numpy array & dict
+series1 = pd.Series()
