a3633a221f1dac66b8a20b98ae47d722552a3d08
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-09 15:54:59 +0530

    Python excercise in pandas on 09-03-2019

--- python/10_pandas/machinelearningplus/5_how_to_assign_name_to_the_series_index?.py
+++ python/10_pandas/machinelearningplus/5_how_to_assign_name_to_the_series_index?.py
@@ -1,3 +1,13 @@
 """
 Problem Statement : 5. How to assign name to the series index?
 """
+# Import pandas as pd
+import pandas as pd
+
+# Initializing Series
+series1 = pd.Series(list("abcedfghijklmnopqrstuvwxyz"))
+
+# Assign name to the series index as 'alphabets'
+series1.name = "alphabets"
+print("Assign name to the series index as 'alphabets':")
+print(series1.head())
