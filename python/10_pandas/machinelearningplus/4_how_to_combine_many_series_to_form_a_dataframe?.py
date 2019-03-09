0c5c10c6cd7ce1df99006222a180dcbc6cf423b5
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-09 15:35:55 +0530

    Python excercise in pandas on 09-03-2019

--- python/10_pandas/machinelearningplus/4_how_to_combine_many_series_to_form_a_dataframe?.py
+++ python/10_pandas/machinelearningplus/4_how_to_combine_many_series_to_form_a_dataframe?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 4. How to combine many series to form a dataframe?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initalizing series1 , series2
+series1 = pd.Series(list("abcedfghijklmnopqrstuvwxyz"))
+series2 = pd.Series(np.arange(26))
+
+# Method 1 : Combining many Series to form a DataFrame
+print("Method 1 : Combining many Series to form a DataFrame")
+print(pd.concat([series1, series2], axis = 1).head())
+
+# Method 2 : Combining many Series to form a DataFrame
+print("\nMethod 2 : Combining many Series to form a DataFrame")
+print(pd.DataFrame({'col1' : series1, 'col2' : series2}).head())
