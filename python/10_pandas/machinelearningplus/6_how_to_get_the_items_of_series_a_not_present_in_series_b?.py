f2d446286350d9fea49fc4709c3a9aef57a9cd6b
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-09 16:08:29 +0530

    Python excercise in pandas on 09-03-2019

--- python/10_pandas/machinelearningplus/6_how_to_get_the_items_of_series_a_not_present_in_series_b?.py
+++ python/10_pandas/machinelearningplus/6_how_to_get_the_items_of_series_a_not_present_in_series_b?.py
@@ -1,3 +1,35 @@
 """
 Problem Statement : 6. How to get the items of series A not present in series B?
 """
+
+# Import pandas as pd
+import pandas as pd
+
+# Initialize series1 & series2
+series1 = pd.Series([1, 2, 3, 4, 5])
+series2 = pd.Series([4, 5, 6, 7, 8])
+
+# Printing series1 & series2
+print("Series1:")
+print(series1)
+print("\nSeries2:")
+print(series2)
+
+# Getting the items of series 1 not present in series 2
+print("\nGetting the items of series 1 not present in series 2:")
+print(series1[~series1.isin(series2)])
+
+"""
+isin() method:
+
+Pandas isin() method is used to filter data frames. isin() method helps in selecting rows with having a particular(or Multiple) value in a particular column.
+
+Syntax:
+DataFrame.isin(values)
+
+Parameters:
+values: iterable, Series, List, Tuple, DataFrame or dictionary to check in the caller Series/Data Frame.
+
+Return Type: 
+DataFrame of Boolean of Dimension.
+"""
