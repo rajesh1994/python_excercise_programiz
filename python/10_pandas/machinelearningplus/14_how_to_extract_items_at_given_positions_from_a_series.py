4b52c5e4ccd875d1733c4cc7a251ad88bee51217
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-11 17:37:16 +0530

    Python excercise in pandas on 09-03-2019 - Ex14

--- python/10_pandas/machinelearningplus/14_how_to_extract_items_at_given_positions_from_a_series.py
+++ python/10_pandas/machinelearningplus/14_how_to_extract_items_at_given_positions_from_a_series.py
@@ -1,3 +1,14 @@
 """
 Problem Statement : 14. How to extract items at given positions from a series
 """
+
+# Import pandas as pd
+import pandas as pd
+
+# Initialize series1
+series1 = pd.Series(list("abcdefghijklmnopqrstuvwxyz"))
+position = [0, 8, 12, 18, 17, 26]
+
+# Extracting items at 0, 8, 12, 18, 17, 26 positions from a series
+print("Extracting items at 0, 8, 12, 18, 17, 26 positions from a series:")
+print(series1.take(pos))
