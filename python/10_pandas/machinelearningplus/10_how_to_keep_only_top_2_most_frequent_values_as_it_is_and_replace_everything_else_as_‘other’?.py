07f7d8be1def6c11956076cd4c24ffb5ef97f323
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-11 12:13:25 +0530

    Python excercise in pandas on 09-03-2019 - Ex10

--- python/10_pandas/machinelearningplus/10_how_to_keep_only_top_2_most_frequent_values_as_it_is_and_replace_everything_else_as_‘other’?.py
+++ python/10_pandas/machinelearningplus/10_how_to_keep_only_top_2_most_frequent_values_as_it_is_and_replace_everything_else_as_‘other’?.py
@@ -1,3 +1,18 @@
 """
 Problem Statement : 10. How to keep only top 2 most frequent values as it is and replace everything else as ‘Other’?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize series1
+np.random.RandomState(100)
+series1 = pd.Series(np.random.randint(1, 5, [12]))
+#print(series1)
+
+# Keep only top 2 most frequent values
+print("Keep only top 2 most frequent values:", series1.value_counts())
+series1[~series1.isin(series1.value_counts().index[:2])] = "Other"
+print(series1)
