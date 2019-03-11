24f626c16231db195ad7a247fe026d273e5d353e
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-11 12:00:33 +0530

    Python excercise in pandas on 09-03-2019 - Ex9

--- python/10_pandas/machinelearningplus/9_how_to_get_frequency_counts_of_unique_items_of_a_series?.py
+++ python/10_pandas/machinelearningplus/9_how_to_get_frequency_counts_of_unique_items_of_a_series?.py
@@ -9,7 +9,8 @@ import numpy as np
 
 # Initilize series1
 series1 = pd.Series(np.take(list("abcdefghi"), np.random.randint(8, size = 30)))
-
+print("Series1:")
+print(series1)
 # Calculating the frequency value of unique items of a series
-print("Calculating the frequency value of unique items of a series:")
-print(series1.values_counts())
+print("\nCalculating the frequency value of unique items of a series:")
+print(series1.value_counts())
