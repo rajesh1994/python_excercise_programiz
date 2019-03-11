ed2b3330d4394c9e6b22c25fb412cd9fbd0f0232
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-11 17:12:15 +0530

    Python excercise in pandas on 09-03-2019 - Ex12

--- python/10_pandas/machinelearningplus/12_how_to_convert_a_numpy_array_to_a_dataframe_of_given_shape?_(l1).py
+++ python/10_pandas/machinelearningplus/12_how_to_convert_a_numpy_array_to_a_dataframe_of_given_shape?_(l1).py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 12. How to convert a numpy array to a dataframe of given shape? (L1)
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize numpy array
+series1 = pd.Series(np.random.randint(1, 10, 35))
+print(series1)
+
+# Reshape the series series1 into a dataframe with 7 rows and 5 columns
+df = pd.DataFrame(series1.values.reshape(5, 7))
+print("Reshape the series series1 into a dataframe with 7 rows and 5 columns:")
+print(df)
