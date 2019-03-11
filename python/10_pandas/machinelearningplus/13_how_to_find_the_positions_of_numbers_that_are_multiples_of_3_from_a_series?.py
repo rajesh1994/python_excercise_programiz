fe1eaa44da6bd353a05253c7058e494b1cdd5c88
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-11 17:18:46 +0530

    Python excercise in pandas on 09-03-2019 - Ex13

--- python/10_pandas/machinelearningplus/13_how_to_find_the_positions_of_numbers_that_are_multiples_of_3_from_a_series?.py
+++ python/10_pandas/machinelearningplus/13_how_to_find_the_positions_of_numbers_that_are_multiples_of_3_from_a_series?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 13. How to find the positions of numbers that are multiples of 3 from a series?
 """
+
+# Import numpy as np
+# Import pandads as pd
+import numpy as np
+import pandas as pd
+
+# Initalize series1
+series1 = pd.Series(np.random.randint(1, 10, 7))
+print("series1:")
+print(series1)
+
+# Finding the numbers that are multiples of 3 from a series
+print("Finding the numbers that are multiples of 3 from a series:")
+print(np.argwhere(series1 % 3 == 0))
