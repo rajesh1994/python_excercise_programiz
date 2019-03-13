2111045c4ee84ca0c00d4fe90588ecccec0f4078
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 12:01:59 +0530

    Python excercise in pandas on 09-03-2019 - Ex15

--- python/10_pandas/machinelearningplus/15_how_to_stack_two_series_vertically_and_horizontally_?.py
+++ python/10_pandas/machinelearningplus/15_how_to_stack_two_series_vertically_and_horizontally_?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 15. How to stack two series vertically and horizontally ?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize series1 & series2
+series1 = pd.Series(np.arange(5))
+series2 = pd.Series(list("abcde"))
+
+# Stack two series vertically
+print("Stack two series vertically:")
+print(series1.append(series2))
+
+# Stack two series horizontally
+print("Stack two series horizontally:")
+print(pd.concat([series1, series2], axis = 1))
--- python/10_pandas/machinelearningplus/16_how_to_get_the_positions_of_items_of_series_a_in_another_series_b?.py
+++ python/10_pandas/machinelearningplus/16_how_to_get_the_positions_of_items_of_series_a_in_another_series_b?.py
@@ -1,3 +1,12 @@
 """
 Problem Statement : 16. How to get the positions of items of series A in another series B?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize series1 & series2
+series1 = pd.Series()
+series2 = pd.Series()
