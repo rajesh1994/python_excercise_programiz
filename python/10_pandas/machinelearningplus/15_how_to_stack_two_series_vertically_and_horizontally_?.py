3860b58ecc456845596e70fcee9033a989b5d2a9
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 12:39:54 +0530

    Python excercise in pandas on 09-03-2019 - Ex16

--- python/10_pandas/machinelearningplus/15_how_to_stack_two_series_vertically_and_horizontally_?.py
+++ python/10_pandas/machinelearningplus/15_how_to_stack_two_series_vertically_and_horizontally_?.py
@@ -10,11 +10,15 @@ import pandas as pd
 # Initialize series1 & series2
 series1 = pd.Series(np.arange(5))
 series2 = pd.Series(list("abcde"))
+print("Series1:")
+print(series1)
+print("\nSeries2:")
+print(series2)
 
 # Stack two series vertically
-print("Stack two series vertically:")
+print("\nStack two series vertically:")
 print(series1.append(series2))
 
 # Stack two series horizontally
-print("Stack two series horizontally:")
+print("\nStack two series horizontally:")
 print(pd.concat([series1, series2], axis = 1))
--- python/10_pandas/machinelearningplus/16_how_to_get_the_positions_of_items_of_series_a_in_another_series_b?.py
+++ python/10_pandas/machinelearningplus/16_how_to_get_the_positions_of_items_of_series_a_in_another_series_b?.py
@@ -8,5 +8,17 @@ import numpy as np
 import pandas as pd
 
 # Initialize series1 & series2
-series1 = pd.Series()
-series2 = pd.Series()
+series1 = pd.Series([10, 9, 6, 5, 3, 1, 12, 8, 13])
+series2 = pd.Series([1, 3, 10, 13])
+print("Series1:")
+print(series1)
+print("\nSeries2:")
+print(series2)
+
+# Method 1 : Getting the postions of items of series1 in another series2
+print("\nMethod 1 : Getting the postions of items of series1 in another series2:")
+print([np.where(i == series1)[0].tolist()[0] for i in series2])
+
+# Method 2 : Getting the postions of items of series1 in another series2
+print("\nMethod 2 : Getting the postions of items of series1 in another series2:")
+print([pd.Index(series1).get_loc(i) for i in series2])
