0fb428441e5a896d9dd98be6bdfe1bdbb26c8cd4
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 13:11:14 +0530

    Python excercise in pandas on 09-03-2019 - Ex17

--- python/10_pandas/machinelearningplus/17_how_to_compute_the_mean_squared_error_on_a_truth_and_predicted_series?.py
+++ python/10_pandas/machinelearningplus/17_how_to_compute_the_mean_squared_error_on_a_truth_and_predicted_series?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 17. How to compute the mean squared error on a truth and predicted series?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize truth & predicted
+truth = pd.Series(np.arange(10))
+predicted = pd.Series(np.arange(10) + np.random.random(10))
+print("Series truth :")
+print(truth)
+print("\nSeries Predicted:")
+print(predicted)
+
+# Computing the mean squared error on a truth & predicted
+print("\nComputing the mean squared error on a truth & predicted:")
+print(np.mean((truth - predicted) ** 2))
