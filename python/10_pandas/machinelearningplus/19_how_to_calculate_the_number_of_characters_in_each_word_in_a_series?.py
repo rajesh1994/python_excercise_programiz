dd6e85a903fc7b4f3c321c66d70a033cdceb9485
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 14:39:24 +0530

    Python excercise in pandas on 09-03-2019 - Ex19

--- python/10_pandas/machinelearningplus/19_how_to_calculate_the_number_of_characters_in_each_word_in_a_series?.py
+++ python/10_pandas/machinelearningplus/19_how_to_calculate_the_number_of_characters_in_each_word_in_a_series?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 19. How to calculate the number of characters in each word in a series?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize series1
+series1 = pd.Series(["Love", "India", "Python", "Chemistry"])
+print("Series1:")
+print(series1)
+
+# Number of characters in each word
+print("\nNumber of characters in each word:")
+print(series1.map(lambda x : len(x)))
