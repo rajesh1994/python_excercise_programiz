996a06eb2d1217b6708758073eeb29e10023ad92
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 15:08:34 +0530

    Python excercise in pandas on 09-03-2019 - Ex20

--- python/10_pandas/machinelearningplus/20_how_to_compute_difference_of_differences_between_consequtive_numbers_of_a_series?.py
+++ python/10_pandas/machinelearningplus/20_how_to_compute_difference_of_differences_between_consequtive_numbers_of_a_series?.py
@@ -1,3 +1,13 @@
 """
 Problem Statement : 20. How to compute difference of differences between consequtive numbers of a series?
 """
+
+# Import pandas as pd
+import pandas as pd
+
+# Initialize series1
+series1 = pd.Series([1, 4, 5, 3, 8, 1, 10, 12, 22, 26])
+
+print("Compute difference of differences between consequtive numbers of a series:")
+print(series1.diff().tolist())
+print(series1.diff().diff().tolist())
