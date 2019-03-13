37987a27abdb361c78ed96b98e09e984624a8e25
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-13 14:29:52 +0530

    Python excercise in pandas on 09-03-2019 - Ex18

--- python/10_pandas/machinelearningplus/18_how_to_convert_the_first_character_of_each_element_in_a_series_to_uppercase?.py
+++ python/10_pandas/machinelearningplus/18_how_to_convert_the_first_character_of_each_element_in_a_series_to_uppercase?.py
@@ -1,3 +1,23 @@
 """
 Problem Statement : 18. How to convert the first character of each element in a series to uppercase?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize series1
+series1 = pd.Series(["india", "england", "sri lanka"])
+print("Series1:")
+print(series1)
+
+# Converting the first character of each element in a series to uppercase
+print("\nMethod 1: Converting the first character of each element in a series to uppercase")
+print(series1.map(lambda x : x.title()))
+
+print("\nMethod 2: Converting the first character of each element in a series to uppercase")
+print(series1.map(lambda x : x[0].upper() + x[1 : ]))
+
+print("\nMethod 3: Converting the first character of each element in a series to uppercase")
+print(pd.Series([i.title() for i in series1]))
