7c49c160664cc2bb28180cddd6a04a66666cac1c
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-19 13:12:28 +0530

    Python excercise in pandas on 09-03-2019 - Ex21

--- python/10_pandas/machinelearningplus/21_how_to_convert_a_series_of_date-strings_to_a_timeseries?.py
+++ python/10_pandas/machinelearningplus/21_how_to_convert_a_series_of_date-strings_to_a_timeseries?.py
@@ -1,3 +1,21 @@
 """
 Problem Statement : 21. How to convert a series of date-strings to a timeseries?
 """
+
+# Import pandas as pd
+# Import parse from dateutil
+import pandas as pd
+from dateutil.parser import parse
+
+# Initilize Series1
+series1 = pd.Series(['01 Jan 2010', '02-02-2002', '20100202', '04/12/2019', '2015-06-06T12:20'])
+print("series1:")
+print(series1)
+
+# Method 1 : converting a series of date-strings to a timeseries
+print("\nMethod 1 : converting a series of date-strings to a timeseries")
+print(series1.map(lambda x : parse(x)))
+
+# Method 2 : converting a series of date-strings to a timeseries
+print("\nMethod 2 : converting a series of date-strings to a timeseries")
+print(pd.to_datetime(series1))
