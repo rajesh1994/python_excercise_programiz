03f3ab2a6f2dbb012afc1271e84ab4763225e20d
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 14:41:05 +0530

    Python excercise in pandas on 09-03-2019 - Ex23

--- python/10_pandas/machinelearningplus/23_how_to_convert_year-month_string_to_dates_corresponding_to_the_4th_day_of_the_month?.py
+++ python/10_pandas/machinelearningplus/23_how_to_convert_year-month_string_to_dates_corresponding_to_the_4th_day_of_the_month?.py
@@ -1,3 +1,26 @@
 """
 Problem Statement : 23. How to convert year-month string to dates corresponding to the 4th day of the month?
 """
+
+# Import pandas as pd
+# Import parse from dateutil.parser
+import pandas as pd
+from dateutil.parser import parse
+
+# Intialize series1
+series1 = pd.Series(['Jan 2019', 'Mar 1994', 'May 1998', 'Oct 2000', 'Dec 2015'])
+print("Series1:")
+print(series1)
+
+# Method 1: convert year-month string to dates corresponding to the 4th day of the month
+print("\nMethod 1: convert year-month string to dates corresponding to the 4th day of the month")
+print(series1.map(lambda x : parse('04' + x)))
+
+# Method 2: convert year-month string to dates corresponding to the 4th day of the month
+# Parse the date
+parse_date = series1.map(lambda x : parse(x))
+
+# Construct date string with date as 4
+series_datestr = parse_date.dt.year.astype('str') + '-' + parse_date.dt.month.astype('str') + '-' + '04'
+print("\nMethod 2: convert year-month string to dates corresponding to the 4th day of the month")
+print(series_datestr)
