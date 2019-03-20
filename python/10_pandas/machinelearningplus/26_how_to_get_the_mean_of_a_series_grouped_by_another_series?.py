3c578af4304cb1d0fceba069c1cdc8df99bff54a
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 16:06:35 +0530

    Python excercise in pandas on 09-03-2019 - Ex26

--- python/10_pandas/machinelearningplus/26_how_to_get_the_mean_of_a_series_grouped_by_another_series?.py
+++ python/10_pandas/machinelearningplus/26_how_to_get_the_mean_of_a_series_grouped_by_another_series?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 26. How to get the mean of a series grouped by another series?
 """
+
+# Import pandas as pd
+# Import numpy as np
+import pandas as pd
+import numpy as np
+
+# Intialize fruits & weight
+fruits = pd.Series(np.random.choice(['Apple', 'Banana', 'Carrot', 'Lime'], 10))
+weights = pd.Series(np.linspace(1, 10, 10))
+print('fruits:')
+print(fruits)
+print('weights:')
+print(weights)
+
+# Mean of weights of each fruit
+print("Mean of weights of each fruit:")
+print(weights.groupby(fruits).mean())
