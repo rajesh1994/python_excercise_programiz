c18bca081f76d7a7433f7b6c5b88b134a151dea0
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 14:53:43 +0530

    Python excercise in pandas on 09-03-2019 - Ex24

--- python/10_pandas/machinelearningplus/24_how_to_filter_words_that_contain_atleast_2_vowels_from_a_series?.py
+++ python/10_pandas/machinelearningplus/24_how_to_filter_words_that_contain_atleast_2_vowels_from_a_series?.py
@@ -1,3 +1,18 @@
 """
 Problem Statement : 24. How to filter words that contain atleast 2 vowels from a series?
 """
+
+# Import pandas as pd
+# Import Counter from collections
+import pandas as pd
+from collections import Counter
+
+# Initialize series1
+series1 = pd.Series(['Apple', 'Banana', 'Juice', 'Plan', 'Python', 'Money'])
+print("series1:")
+print(series1)
+
+# filter words that contain atleast 2 vowels from a series
+mask = series1.map(lambda x : sum([Counter(x.lower()).get(i, 0) for i in list('aeiou')]) >= 2)
+print("\nFilter words that contain atleast 2 vowels from a series:")
+print(series1[mask])
