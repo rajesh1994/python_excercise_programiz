c8bea2b9a7215a2804daca7337a3b2f1fad3398a
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 17:52:58 +0530

    Python excercise in pandas on 20-03-2019 - Ex28

--- python/10_pandas/machinelearningplus/28_how_to_find_all_the_local_maxima_(or_peaks)_in_a_numeric_series?.py
+++ python/10_pandas/machinelearningplus/28_how_to_find_all_the_local_maxima_(or_peaks)_in_a_numeric_series?.py
@@ -1,3 +1,19 @@
 """
 Problem Statement : 28. How to find all the local maxima (or peaks) in a numeric series?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Intialize series1
+series1 = pd.Series([2, 10, 3, 4, 9, 10, 2, 7, 3])
+print("series1:")
+print(series1)
+
+# Find all the local maxima (or peaks) in a numeric series
+dd = np.diff(np.sign(np.diff(series1)))
+peak_locks = np.where(dd == -2)[0] + 1
+print("\nPositions of peaks (values surrounded by smaller values on both sides) in series1:")
+print(peak_locks)
