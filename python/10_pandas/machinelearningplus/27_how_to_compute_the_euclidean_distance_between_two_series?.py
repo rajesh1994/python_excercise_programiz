4484626ef160977d2b45e21471c15c66a64b6548
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 17:31:39 +0530

    Python excercise in pandas on 20-03-2019 - Ex27

--- python/10_pandas/machinelearningplus/27_how_to_compute_the_euclidean_distance_between_two_series?.py
+++ python/10_pandas/machinelearningplus/27_how_to_compute_the_euclidean_distance_between_two_series?.py
@@ -1,3 +1,27 @@
 """
 Problem Statement : 27. How to compute the euclidean distance between two series?
 """
+
+# Import pandas as pd
+# Import numpy as np
+import pandas as pd
+import numpy as np
+
+# Initialize series1 & series2
+series1 = pd.Series([i for i in range(10)])
+series2 = pd.Series([i for i in range(9, -1, -1)])
+
+"""
+print('series1:')
+print(series1)
+print('\nseries2:')
+print(series2)
+"""
+
+# Method 1: compute the euclidean distance between two series
+print("\nMethod 1: Euclidean distance between two series")
+print(round(sum((series1 - series2)**2)**.5, 2))
+
+# Method 2: Euclidean distance between two series
+print("\nMethod 2: Euclidean distance between two series")
+print(round(np.linalg.norm(series1 - series2), 2))
