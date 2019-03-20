4f3b08fe9f113ac1e44bb37a277f19b1de5d2740
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-20 18:07:17 +0530

    Python excercise in pandas on 20-03-2019 - Ex29

--- python/10_pandas/machinelearningplus/29_how_to_replace_missing_spaces_in_a_string_with_the_least_frequent_character?.py
+++ python/10_pandas/machinelearningplus/29_how_to_replace_missing_spaces_in_a_string_with_the_least_frequent_character?.py
@@ -1,3 +1,15 @@
 """
 Problem Statement : 29. How to replace missing spaces in a string with the least frequent character?
 """
+# Import pandas as pd
+import pandas as pd
+
+# Initialize series1
+series1 =  pd.Series(list('dbc deb abed gade'))
+freq = series1.value_counts()
+print(freq)
+least_freq = freq.dropna().index[-1]
+print(least_freq)
+
+print("\nReplace missing spaces in a string with the least frequent character:")
+print("".join(series1.replace(' ', least_freq)))
