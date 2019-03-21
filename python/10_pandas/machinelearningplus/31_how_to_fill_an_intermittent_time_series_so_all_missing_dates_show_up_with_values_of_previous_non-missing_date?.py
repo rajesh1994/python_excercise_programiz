aaaa8fd6a205b49aa996c45635f5d5424a28338c
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-21 18:17:44 +0530

    Python excercise in pandas on 21-03-2019 - Ex31

--- python/10_pandas/machinelearningplus/31_how_to_fill_an_intermittent_time_series_so_all_missing_dates_show_up_with_values_of_previous_non-missing_date?.py
+++ python/10_pandas/machinelearningplus/31_how_to_fill_an_intermittent_time_series_so_all_missing_dates_show_up_with_values_of_previous_non-missing_date?.py
@@ -1,3 +1,14 @@
 """
 Problem Statement : 31. How to fill an intermittent time series so all missing dates show up with values of previous non-missing date?
 """
+
+# Import pandas as pd
+# Import numpy as np
+import pandas as pd
+import numpy as np
+
+# Initialize series1
+series1 = pd.Series([1, 10, 22, np.nan], index = pd.to_datetime(['2000-01-01', '2000-01-03', '2000-01-06', '2000-01-08']))
+
+# Solution
+print(series1.resample('D').ffill())
