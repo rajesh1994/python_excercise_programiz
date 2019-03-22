7da1164647ebe6ff3d499bceadd80e09f453aaa0
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-22 18:39:40 +0530

    Python excercise in pandas on 22-03-2019 - Ex37

--- python/10_pandas/machinelearningplus/37_how_to_get_the_nrows,_ncolumns,_datatype,_summary_stats_of_each_column_of_a_dataframe?_also_get_the_array_and_list_equivalent.py
+++ python/10_pandas/machinelearningplus/37_how_to_get_the_nrows,_ncolumns,_datatype,_summary_stats_of_each_column_of_a_dataframe?_also_get_the_array_and_list_equivalent.py
@@ -2,3 +2,36 @@
 Problem Statement : 37. How to get the nrows, ncolumns, datatype, summary stats of each column of a dataframe? Also get the array and list equivalent.
 """
 
+# Import pandas as pd
+# Import numpy as np
+import pandas as pd
+import numpy as np
+
+# Reading the csv file
+df = pd.read_csv('cars93.csv')
+
+# Number of rows & columns
+print("Number of rows & columns:")
+print(df.shape)
+
+# Data-type
+print("\nData-type:")
+print(df.dtypes)
+
+# How many columns under each datatype
+print("\nHow many columns under each datatype:")
+print(df.get_dtype_counts())
+print(df.dtypes.value_counts())
+
+# Summary statistics
+print("\nSummary statistics:")
+print(df.describe())
+
+# Numpy array
+print("\nNumpy array:")
+print(df.values)
+"""
+# List
+print("\nList:")
+print(df.values.tolist())
+"""
