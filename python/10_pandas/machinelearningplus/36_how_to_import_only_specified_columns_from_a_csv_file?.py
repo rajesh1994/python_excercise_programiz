e7e517010518b837b4c1ace3d34d6da65275eb00
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-22 16:58:46 +0530

    Python excercise in pandas on 22-03-2019 - Ex36

--- python/10_pandas/machinelearningplus/36_how_to_import_only_specified_columns_from_a_csv_file?.py
+++ python/10_pandas/machinelearningplus/36_how_to_import_only_specified_columns_from_a_csv_file?.py
@@ -1,3 +1,11 @@
 """
 Problem Statement : 36. How to import only specified columns from a csv file?
 """
+
+# Import pandas as pd
+import pandas as pd
+
+# Import 'rm', 'age', 'medv' columns from a csv file
+df1 = pd.read_csv('boston_housing.csv', usecols = ['rm', 'age', 'medv'])
+print("Import 'rm', 'age', 'medv' columns from a csv file:")
+print(df1.head())
