5fcbdde4419e1f278147f33571a4409f941d33b7
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-23 16:13:22 +0530

    Python excercise in pandas on 22-03-2019 - Ex44

--- python/10_pandas/machinelearningplus/44_how_to_select_a_specific_column_from_a_dataframe_as_a_dataframe_instead_of_a_series?.py
+++ python/10_pandas/machinelearningplus/44_how_to_select_a_specific_column_from_a_dataframe_as_a_dataframe_instead_of_a_series?.py
@@ -8,4 +8,17 @@ import numpy as np
 import pandas as pd
 
 # Initialize DataFrame 'df1'
-df1 = pd.DataFrame(np.arange(20))
+df1 = pd.DataFrame(np.arange(20).reshape(-1, 5), columns = [list('abcde')])
+
+# First column in df1 as a DataFrame
+print("First column in df1 as a DataFrame:")
+print(type([['a']]))
+print(type(df1.loc[:, ['a']]))
+print(type(df1.iloc[:, [0]]))
+
+# First column in df1 as a series
+print("First column in df1 as a series:")
+print(type(df1.a))
+print(type(df1['a']))
+print(type(df1.loc[:, 'a']))
+print(type(df1.iloc[:, 1]))
