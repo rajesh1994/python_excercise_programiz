d91345b34b0ee1139bb93eeaf49c89cddc84ca48
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-23 15:23:13 +0530

    Python excercise in pandas on 22-03-2019 - Ex43

--- python/10_pandas/machinelearningplus/43_how_to_use_apply_function_on_existing_columns_with_global_variables_as_additional_arguments?.py
+++ python/10_pandas/machinelearningplus/43_how_to_use_apply_function_on_existing_columns_with_global_variables_as_additional_arguments?.py
@@ -2,3 +2,16 @@
 Problem Statement : 43. How to use apply function on existing columns with global variables as additional arguments?
 """
 
+# Import pandas as pd
+# import numpy as np
+import pandas as pd
+import numpy as np
+
+# Read data from CSV file
+df1 = pd.read_csv('cars93.csv', usecols = ['Min.Price', 'Max.Price'])
+print("Data from the CSV file:")
+print(df1.head())
+
+d = {'Min.Price' : np.nanmean, 'Max.Price' : np.nanmedian}
+df1[['Min.Price', 'Max.Price']] = df1[['Min.Price', 'Max.Price']].apply(lambda x, d : x.fillna(d[x.name](x)), args = (d, ))
+print(df1[['Min.Price', 'Max.Price']].head())
--- python/10_pandas/machinelearningplus/44_how_to_select_a_specific_column_from_a_dataframe_as_a_dataframe_instead_of_a_series?.py
+++ python/10_pandas/machinelearningplus/44_how_to_select_a_specific_column_from_a_dataframe_as_a_dataframe_instead_of_a_series?.py
@@ -2,3 +2,10 @@
 Problem Statement : 44. How to select a specific column from a dataframe as a dataframe instead of a series?
 """
 
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df1'
+df1 = pd.DataFrame(np.arange(20))
