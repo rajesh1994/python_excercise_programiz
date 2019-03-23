93a850ed7f0d6505d93b05a903fa3f8cbc11fc23
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-23 12:49:38 +0530

    Python excercise in pandas on 22-03-2019 - Ex40

--- python/10_pandas/machinelearningplus/40_how_to_check_if_a_dataframe_has_any_missing_values?.py
+++ python/10_pandas/machinelearningplus/40_how_to_check_if_a_dataframe_has_any_missing_values?.py
@@ -2,3 +2,12 @@
 Problem Statement : 40. How to check if a dataframe has any missing values?
 """
 
+# Import pandas as pd
+import pandas as pd
+
+# Reading data from the CSV file
+df1 = pd.read_csv('cars93.csv')
+#print(df1)
+# Check if df has any missing values
+print("Check if df has any missing values:")
+print(df1.isnull())
