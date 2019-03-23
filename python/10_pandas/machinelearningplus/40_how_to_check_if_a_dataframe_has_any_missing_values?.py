393ab67f0a5338b924e06a7380880cadaf59457e
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-23 14:01:21 +0530

    Python excercise in pandas on 22-03-2019 - Ex41

--- python/10_pandas/machinelearningplus/40_how_to_check_if_a_dataframe_has_any_missing_values?.py
+++ python/10_pandas/machinelearningplus/40_how_to_check_if_a_dataframe_has_any_missing_values?.py
@@ -6,8 +6,10 @@ Problem Statement : 40. How to check if a dataframe has any missing values?
 import pandas as pd
 
 # Reading data from the CSV file
+print("Data from the CSV file:")
 df1 = pd.read_csv('cars93.csv')
-#print(df1)
+print(df1.head())
+
 # Check if df has any missing values
 print("Check if df has any missing values:")
-print(df1.isnull())
+print(df1.isnull().head())
--- python/10_pandas/machinelearningplus/41_how_to_count_the_number_of_missing_values_in_each_column?.py
+++ python/10_pandas/machinelearningplus/41_how_to_count_the_number_of_missing_values_in_each_column?.py
@@ -2,3 +2,13 @@
 Problem Statement : 41. How to count the number of missing values in each column?
 """
 
+# Import pandas as pd
+import pandas as pd
+
+# Read data from the CSV file
+df1 = pd.read_csv('cars93.csv')
+
+# Number of missing values in each column
+num_missing_values_each_cols = df1.apply(lambda x : x .isnull().sum())
+print("Number of missing values in each column:")
+print(num_missing_values_each_cols.argmax())
