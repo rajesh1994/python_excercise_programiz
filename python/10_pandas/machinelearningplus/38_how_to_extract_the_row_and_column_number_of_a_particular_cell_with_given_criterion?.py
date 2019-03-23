e2b37d8eb0ab51a53f3969c31532f55151420a37
rajesh1994 <rajeshsurya1994@gmail.com>
2019-03-23 11:27:54 +0530

    Python excercise in pandas on 22-03-2019 - Ex38

--- python/10_pandas/machinelearningplus/38_how_to_extract_the_row_and_column_number_of_a_particular_cell_with_given_criterion?.py
+++ python/10_pandas/machinelearningplus/38_how_to_extract_the_row_and_column_number_of_a_particular_cell_with_given_criterion?.py
@@ -2,3 +2,28 @@
 Problem Statement : 38. How to extract the row and column number of a particular cell with given criterion?
 """
 
+# Import pandas as pd
+# Import numpy as np
+import pandas as pd
+import numpy as np
+
+# Reading the csv file
+df1 = pd.read_csv('cars93.csv')
+
+# Get manufacturer with highest price
+print("\nManufacturer with highest price:")
+print(df1.loc[df1.Price == np.max(df1.Price), ['Manufacturer', 'Model', 'Type']])
+
+# Get row & column number
+print("\nRow & column number of highest price:")
+row, col = np.where(df1.values == np.max(df1.Price))
+print(row, col)
+
+# Get the values of highest price
+print("\nValues of highest price:")
+print(df1.iat[row[0], col[0]])
+print(df1.iloc[row[0], col[0]])
+
+# Alternates
+#print(df1.at[row[0], 'Price'])
+#print(df1.get_value(row[0], 'Price'))
