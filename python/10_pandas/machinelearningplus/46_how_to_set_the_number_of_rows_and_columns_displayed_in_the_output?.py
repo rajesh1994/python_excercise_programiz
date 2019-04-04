53c6779c4aba1470a8b67f02586e51d8608e415f
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-04 16:40:29 +0530

    Python excercise in pandas on 22-03-2019 - Ex46

--- python/10_pandas/machinelearningplus/46_how_to_set_the_number_of_rows_and_columns_displayed_in_the_output?.py
+++ python/10_pandas/machinelearningplus/46_how_to_set_the_number_of_rows_and_columns_displayed_in_the_output?.py
@@ -1,4 +1,17 @@
 """
 Problem Statement : 46. How to set the number of rows and columns displayed in the output?
 """
+# Import pandas as pd
+import pandas as pd
 
+# Reading the CSV file
+df1 = pd.read_csv('cars93.csv')
+
+# Change the pandas display settings on printing the dataframe df it shows a maximum of 2 rows and 2 columns.
+print("setting the 2 rows & 2 columns to be displayed in the output:")
+pd.set_option("display.max_columns", 2)
+pd.set_option("display.max_rows", 2)
+print(df1)
+
+# Show all available options
+# pd.describe_option()
