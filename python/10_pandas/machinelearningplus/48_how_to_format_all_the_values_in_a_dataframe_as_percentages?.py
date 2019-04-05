7780e66a7c8ee068e7a9e09804e5a65d61691c04
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-05 18:27:45 +0530

    Python excercise in pandas on 05-04-2019 - Ex48

--- python/10_pandas/machinelearningplus/48_how_to_format_all_the_values_in_a_dataframe_as_percentages?.py
+++ python/10_pandas/machinelearningplus/48_how_to_format_all_the_values_in_a_dataframe_as_percentages?.py
@@ -1,4 +1,24 @@
 """
 Problem Statement : 48. How to format all the values in a dataframe as percentages?
 """
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+"""
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.random(4), columns = ['random'])
 
+# Format the values in column 'random' of df as percentages.
+out = df.style.format({'random' : '{0:.2%}'.format})
+
+print(out)
+"""
+# Input
+df = pd.DataFrame(np.random.random(4), columns=['random'])
+
+# Solution
+out = df.style.format({
+    'random': '{0:.2%}'.format,
+})
+print(out)
