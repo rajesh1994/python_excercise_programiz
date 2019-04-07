8ee2081527fd5e693d02e412ce1eddc8210ee4a4
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-07 16:06:35 +0530

    Python excercise in pandas on 07-04-2019 - Ex51

--- python/10_pandas/machinelearningplus/51_how_to_get_the_row_number_of_the_nth_largest_value_in_a_column?.py
+++ python/10_pandas/machinelearningplus/51_how_to_get_the_row_number_of_the_nth_largest_value_in_a_column?.py
@@ -1,3 +1,18 @@
 """
 Problem Statement : 51. How to get the row number of the nth largest value in a column?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 30, 30).reshape(10, -1), columns = list('abc'))
+print("DataFrame df is:")
+print(df)
+
+# Find the row position of the 5th largest value of column 'a' in df.
+n = 5
+print("Row position of the 4th largest value of column 'a' in df")
+print(df['a'].argsort()[::-1][n])
