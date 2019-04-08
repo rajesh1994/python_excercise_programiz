d0c4fca3715d4bbdebd50a9d3161927b2be2a5ae
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-08 16:57:24 +0530

    Python excercise in pandas on 08-04-2019 - Ex52

--- python/10_pandas/machinelearningplus/52_how_to_find_the_position_of_the_nth_largest_value_greater_than_a_given_value?.py
+++ python/10_pandas/machinelearningplus/52_how_to_find_the_position_of_the_nth_largest_value_greater_than_a_given_value?.py
@@ -1,3 +1,19 @@
 """
 Problem Statement : 52. How to find the position of the nth largest value greater than a given value?
 """
+
+# Import numpy as np
+# Import pandasa as pd
+import numpy as np
+import pandas as pd
+
+# Defining a Series 's'
+s = pd.Series(np.random.randint(1, 100, 15))
+print("Series 's':")
+print(s.tolist())
+
+# In ser, find the position of the 2nd largest value greater than the mean.
+print("\nMean of the Series:")
+print(round(s.mean()))
+print("Position of the 2nd largest value greater than the mean is:")
+print(np.argwhere(s > s.mean())[1])
