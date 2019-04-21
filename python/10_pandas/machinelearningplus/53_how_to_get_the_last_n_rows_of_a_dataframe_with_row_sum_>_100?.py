9edf9558e1db8d5cee77adeb9ec74edad325cabf
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-21 19:58:18 +0530

    Python excercise in pandas on 21-04-2019 - Ex53

--- python/10_pandas/machinelearningplus/53_how_to_get_the_last_n_rows_of_a_dataframe_with_row_sum_>_100?.py
+++ python/10_pandas/machinelearningplus/53_how_to_get_the_last_n_rows_of_a_dataframe_with_row_sum_>_100?.py
@@ -1,3 +1,21 @@
 """
 Problem Statement : 53. How to get the last n rows of a dataframe with row sum > 100?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initializing DataFrame 'df'
+df = pd.DataFrame(np.random.randint(10, 40, 60).reshape(-1, 4))
+print("DataFrame 'df':")
+print(df.tail())
+
+# Print row-sum
+rowsums = df.apply(np.sum, axis=1)
+#print(rowsums)
+
+# Last two rows with row-sum greater than 100
+last_two_rows = df.iloc[np.where(rowsums > 100)[0][-2], :]
+print(last_two_rows)
