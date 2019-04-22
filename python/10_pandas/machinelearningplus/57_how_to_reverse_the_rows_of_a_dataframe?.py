6126ad481f6acc9cce5cffa6f174c777c25d26b6
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-22 18:25:33 +0530

    Python excercise in pandas on 22-04-2019 - Ex57

--- python/10_pandas/machinelearningplus/57_how_to_reverse_the_rows_of_a_dataframe?.py
+++ python/10_pandas/machinelearningplus/57_how_to_reverse_the_rows_of_a_dataframe?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 57. How to reverse the rows of a dataframe?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.arange(25).reshape(5, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Reverse the rows of a DataFrame
+print("\nReverse the rows of a DataFrame:")
+print(df.iloc[:: -1, :])
