aa7a177080b4f9c5823ba2f1fc7f0527f4d8136b
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-22 17:57:44 +0530

    Python excercise in pandas on 22-04-2019 - Ex56

--- python/10_pandas/machinelearningplus/56_how_to_swap_two_rows_of_a_dataframe?.py
+++ python/10_pandas/machinelearningplus/56_how_to_swap_two_rows_of_a_dataframe?.py
@@ -1,3 +1,21 @@
 """
 Problem Statement : 56. How to swap two rows of a dataframe?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.arange(25).reshape(5, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Solution
+def swap_rows(df, i1, i2):
+	a, b = df.iloc[i1, :].copy(), df.iloc[i2, :].copy()
+	df.iloc[i1, :], df.iloc[i2, :] = b, a
+	return df
+print("\nSwap two rows of a DataFrame:")
+print(swap_rows(df, 1, 2))
