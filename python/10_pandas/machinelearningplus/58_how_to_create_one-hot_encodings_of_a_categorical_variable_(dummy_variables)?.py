845db3ebec73b9c3a0c0b5227abae592314cb56a
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 14:26:58 +0530

    Python excercise in pandas on 23-04-2019 - Ex58

--- python/10_pandas/machinelearningplus/58_how_to_create_one-hot_encodings_of_a_categorical_variable_(dummy_variables)?.py
+++ python/10_pandas/machinelearningplus/58_how_to_create_one-hot_encodings_of_a_categorical_variable_(dummy_variables)?.py
@@ -1,3 +1,18 @@
 """
 Problem Statement : 58. How to create one-hot encodings of a categorical variable (dummy variables)?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import pandas as pd
+import numpy as np
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.arange(25).reshape(5, -1), columns = list('abcde'))
+print("DataFrame 'df':")
+print(df)
+
+# Create one-hot encodings of a categorical variable (dummy variables)
+df_onehot = pd.concat([pd.get_dummies(df['a']), df[list('bcde')]], axis = 1)
+print("\nCreate one-hot encodings of a categorical variable(dummy variables):")
+print(df_onehot)
