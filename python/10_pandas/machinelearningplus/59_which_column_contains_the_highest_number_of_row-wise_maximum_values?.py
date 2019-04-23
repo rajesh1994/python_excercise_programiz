2d5f2f2a31b921625599f55f0eae3d2903ac0337
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 14:44:00 +0530

    Python excercise in pandas on 23-04-2019 - Ex59

--- python/10_pandas/machinelearningplus/59_which_column_contains_the_highest_number_of_row-wise_maximum_values?.py
+++ python/10_pandas/machinelearningplus/59_which_column_contains_the_highest_number_of_row-wise_maximum_values?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 59. Which column contains the highest number of row-wise maximum values?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 40).reshape(10, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Column with highest row maxes
+print("\nColumn with highest row maxes:")
+print(df.apply(np.argmax, axis = 1).value_counts().index[0])
