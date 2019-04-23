535f5318e03af5aa9d425a08a721a1582908d3f2
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 18:03:55 +0530

    Python excercise in pandas on 23-04-2019 - Ex62

--- python/10_pandas/machinelearningplus/62_how_to_create_a_column_containing_the_minimum_by_maximum_of_each_row?.py
+++ python/10_pandas/machinelearningplus/62_how_to_create_a_column_containing_the_minimum_by_maximum_of_each_row?.py
@@ -1,3 +1,23 @@
 """
 Problem Statement : 62. How to create a column containing the minimum by maximum of each row?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 80).reshape(8, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Solution 1 : Minimum by maximum of each row
+min_by_max1 = df.apply(lambda x : np.min(x) / np.max(x), axis = 1)
+print("\nSolution 1 : Minimum by maximum of each row")
+print(round(min_by_max1, 2))
+
+# Solution 2 : Minimum by maximum of each row
+min_by_max2 = np.min(df, axis = 1) / np.max(df, axis = 1)
+print("\nSolution 2 : Minimum by maximum of each row")
+print(round(min_by_max2, 2))
