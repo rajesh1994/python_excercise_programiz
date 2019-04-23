9de57a89d20e765f77b699db54ed14b24bb5aac9
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 16:45:41 +0530

    Python excercise in pandas on 23-04-2019 - Ex60

--- python/10_pandas/machinelearningplus/60_how_to_create_a_new_column_that_contains_the_row_number_of_nearest_column_by_euclidean_distance?.py
+++ python/10_pandas/machinelearningplus/60_how_to_create_a_new_column_that_contains_the_row_number_of_nearest_column_by_euclidean_distance?.py
@@ -1,3 +1,35 @@
 """
 Problem Statement : 60. How to create a new column that contains the row number of nearest column by euclidean distance?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 40).reshape(10, -1), columns = list('pqrs'), index = list('abcdefghij'))
+print("DataFrame 'df':")
+print(df)
+
+# Initialize outputs
+nearest_rows = []
+nearest_distance = []
+
+# Iterate rows
+for i, row in df.iterrows():
+	curr = row
+	rest = df.drop(i)
+	# Initialize dict to store euclidean distance for current row
+	e_dists = {}
+	# Iterate the rest of the rows for current row
+	for j, contestant in rest.iterrows():
+		# Compute euclidean distance & update e_dists
+		e_dists.update({j : round(np.linalg.norm(curr.values - contestant.values))})
+	# Update the nearest row to the current row & the distance value
+	nearest_rows.append(max(e_dists, key = e_dists.get))
+	nearest_distance.append(max(e_dists.values()))
+df['nearest_rows'] = nearest_rows
+df['nearest_distance'] = nearest_distance
+print("\nEach row contains the row number of nearest row-record by euclidean distance:")
+print(df)
