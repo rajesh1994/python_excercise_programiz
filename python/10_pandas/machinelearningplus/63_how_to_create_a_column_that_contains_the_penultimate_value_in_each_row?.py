52f93cd2cc8654ae883e52db8304ebbc9a204828
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 18:22:10 +0530

    Python excercise in pandas on 23-04-2019 - Ex63

--- python/10_pandas/machinelearningplus/63_how_to_create_a_column_that_contains_the_penultimate_value_in_each_row?.py
+++ python/10_pandas/machinelearningplus/63_how_to_create_a_column_that_contains_the_penultimate_value_in_each_row?.py
@@ -1,3 +1,19 @@
 """
 Problem Statement : 63. How to create a column that contains the penultimate value in each row?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 80).reshape(8, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Create a new column 'penultimate' which has the second largest value of each row of df
+out = df.apply(lambda x : x.sort_values().unique()[-2], axis = 1)
+df['penultimate'] = out
+print("\nColumn 'penultimate' which has the second largest value of each row of df:")
+print(df)
