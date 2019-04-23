003a2fe9b2f315a11f682661f46118eb6d1764dc
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-23 18:32:18 +0530

    Python excercise in pandas on 23-04-2019 - Ex65

--- python/10_pandas/machinelearningplus/65_how_to_compute_the_correlation_of_each_row_with_the_suceeding_row?.py
+++ python/10_pandas/machinelearningplus/65_how_to_compute_the_correlation_of_each_row_with_the_suceeding_row?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 65. How to compute the correlation of each row with the suceeding row?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 80).reshape(8, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Correlation of each row of df with its succeeding row
+print("\nCorrelation of each row of df with its succeeding row:")
+print([df.iloc[i].corr(df.iloc[i + 1]).round(2) for i in range(df.shape[0])[:-1]])
