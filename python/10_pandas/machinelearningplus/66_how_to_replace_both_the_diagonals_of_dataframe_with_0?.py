227e6f67745d1e2fff303810840f69e68bf81d28
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-24 16:04:44 +0530

    Python excercise in pandas on 24-04-2019 - Ex66

--- python/10_pandas/machinelearningplus/66_how_to_replace_both_the_diagonals_of_dataframe_with_0?.py
+++ python/10_pandas/machinelearningplus/66_how_to_replace_both_the_diagonals_of_dataframe_with_0?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 66. How to replace both the diagonals of dataframe with 0?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 100).reshape(10, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Replace both the diagonals of dataframe with 0
+for i in range(df.shape[0]):
+	df.iat[i, i] = 0
+	df.iat[df.shape[0] - i - 1, i] = 0
+print("\nReplace both the diagonals of dataframe with 0:")
+print(df)
