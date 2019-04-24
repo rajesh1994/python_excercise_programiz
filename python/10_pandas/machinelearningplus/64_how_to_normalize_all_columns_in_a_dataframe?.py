91bf71f30f8bfe5df7f4cbe78ec7c3cd6fee837b
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-24 15:14:36 +0530

    Python excercise in pandas on 24-04-2019 - Ex64

--- python/10_pandas/machinelearningplus/64_how_to_normalize_all_columns_in_a_dataframe?.py
+++ python/10_pandas/machinelearningplus/64_how_to_normalize_all_columns_in_a_dataframe?.py
@@ -1,3 +1,41 @@
 """
 Problem Statement : 64. How to normalize all columns in a dataframe?
 """
+
+"""
+1. Normalize all columns of df by subtracting the column mean and divide by standard deviation.
+
+2. Range all columns of df such that the minimum value in each column is 0 and max is 1.
+
+3. Don’t use external packages like sklearn.
+
+"""
+# Import numpy as np
+# Import pandas as pd
+import numpy  as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 100, 80).reshape(8, -1))
+print("DataFrame 'df':")
+print(df)
+
+# Normalize all columns of df by subtracting the column mean and divide by standard deviation
+"""
+col_mean = df.apply(lambda x : x.mean().round(2))
+print("\nColumn mean:")
+print(col_mean)
+
+col_std = df.apply(lambda x : x.std().round(2))
+print("\nColumn standard deviation:")
+print(col_std)
+"""
+
+output1 = df.apply(lambda x : ((x - x.mean()) / x.std()).round(2))
+print("\nNormalize all columns of df by subtracting the column mean and divide by standard deviation:")
+print(output1)
+
+# Range all columns of df such that the minimum value in each column is 0 and max is 1
+output2 = df.apply(lambda x : ((x.max() - x) / (x.max() - x.min())).round(2))
+print("\nRange all columns of df such that the minimum value in each column is 0 and max is 1:")
+print(output2)
