99d7c8fc53dabeb5b0f46e65a59c3a1ec1a3b9ae
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-24 17:34:49 +0530

    Python excercise in pandas on 24-04-2019 - Ex70

--- python/10_pandas/machinelearningplus/70_how_to_join_two_dataframes_by_2_columns_so_they_have_only_the_common_rows?.py
+++ python/10_pandas/machinelearningplus/70_how_to_join_two_dataframes_by_2_columns_so_they_have_only_the_common_rows?.py
@@ -1,3 +1,21 @@
 """
 Problem Statement : 70. How to join two dataframes by 2 columns so they have only the common rows?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df1' & 'df2'
+df1 = pd.DataFrame({'fruits' : ['apple', 'banana', 'orange'] * 3, 'weight' : ['high', 'medium', 'low'] * 3, 'price' : np.random.randint(0, 15, 9)})
+
+df2 = pd.DataFrame({'pazham' : ['apple', 'banana', 'orange'] * 2, 'kilo' : ['high', 'low'] * 3, 'price' : np.random.randint(0, 15, 6)})
+print("DataFrame 'df1':")
+print(df1)
+print("\nDataFrame 'df2':")
+print(df2)
+
+# Joining dataframes df1 and df2 by ‘fruit-pazham’ and ‘weight-kilo’
+print("\nJoining dataframes df1 and df2 by ‘fruit-pazham’ and ‘weight-kilo’:")
+print(pd.merge(df1, df2, how = 'inner', left_on = ['fruits', 'weight'], right_on = ['pazham', 'kilo'], suffixes = ['_left', '_right']))
