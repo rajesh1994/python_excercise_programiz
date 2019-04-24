7414b0f5c6e2789be7b79e3350e904d5154a8b3d
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-24 18:21:05 +0530

    Python excercise in pandas on 24-04-2019 - Ex72

--- python/10_pandas/machinelearningplus/72_how_to_create_lags_and_leads_of_a_column_in_a_dataframe?.py
+++ python/10_pandas/machinelearningplus/72_how_to_create_lags_and_leads_of_a_column_in_a_dataframe?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 72. How to create lags and leads of a column in a dataframe?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame({'fruit1' : np.random.choice(['apple', 'banana', 'orange'], 10), 'fruit2' : np.random.choice(['apple', 'banana', 'orange'], 10)})
+print("DataFrame 'df':")
+print(df)
+
+# Positions of where values of two columns match
+print("\nPositions of where values of two columns match:")
+print(np.where(df.fruit1 == df.fruit2))
