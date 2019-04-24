34634c7e78a39f9897cb9ad7cae4d26b97e52f41
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-24 17:00:14 +0530

    Python excercise in pandas on 24-04-2019 - Ex69

--- python/10_pandas/machinelearningplus/69_how_to_compute_grouped_mean_on_pandas_dataframe_and_keep_the_grouped_column_as_another_column_(not_index)?.py
+++ python/10_pandas/machinelearningplus/69_how_to_compute_grouped_mean_on_pandas_dataframe_and_keep_the_grouped_column_as_another_column_(not_index)?.py
@@ -1,3 +1,18 @@
 """
 Problem Statement : 69. How to compute grouped mean on pandas dataframe and keep the grouped column as another column (not index)?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame({'fruits' : ['apple', 'banana', 'orange'] * 3, 'rating' : np.random.rand(9), 'price' : np.random.randint(1, 15, 9)})
+print("DataFrame 'df':")
+print(df)
+
+# In df, Compute the mean price of every fruit, while keeping the fruit as another column instead of an index
+df_out = df.groupby('fruits', as_index = 0)['price'].mean()
+print("\nMean price of every fruit, while keeping the fruit as another column instead of an index:")
+print(df_out)
