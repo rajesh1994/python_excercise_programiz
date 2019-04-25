163c3eeb286c5984e6ec189c1e58fdfce8aa704b
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-25 15:58:45 +0530

    Python excercise in pandas on 25-04-2019 - Ex74

--- python/10_pandas/machinelearningplus/73_how_to_get_the_frequency_of_unique_values_in_the_entire_dataframe?.py
+++ python/10_pandas/machinelearningplus/73_how_to_get_the_frequency_of_unique_values_in_the_entire_dataframe?.py
@@ -1,3 +1,17 @@
 """
 Problem Statement : 73. How to get the frequency of unique values in the entire dataframe?
 """
+
+# Import numpy as np
+# Import pandas as pd
+import numpy as np
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(np.random.randint(1, 10, 20).reshape(-1, 4), columns = list('abcd'))
+print("DataFrame 'df':")
+print(df)
+
+# Frequency of unique values in the entire dataframe
+print("\nFrequency of unique values in the entire dataframe:")
+print(pd.value_counts(df.values.ravel()))
