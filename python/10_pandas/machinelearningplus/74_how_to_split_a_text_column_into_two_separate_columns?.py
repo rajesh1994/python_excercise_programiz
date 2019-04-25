2b700369436da4c76c7354088c0f709e0cbddf3a
rajesh1994 <rajeshsurya1994@gmail.com>
2019-04-25 17:41:00 +0530

    Python excercise in pandas on 25-04-2019 - Ex74

--- python/10_pandas/machinelearningplus/74_how_to_split_a_text_column_into_two_separate_columns?.py
+++ python/10_pandas/machinelearningplus/74_how_to_split_a_text_column_into_two_separate_columns?.py
@@ -1,3 +1,20 @@
 """
 Problem Statement : 74. How to split a text column into two separate columns?
 """
+
+# Import pandas as pd
+import pandas as pd
+
+# Initialize DataFrame 'df'
+df = pd.DataFrame(["STD, City	State", "33, Kolkata	West Bengal", "44, Chennai	Tamil Nadu", "40, Hydrabad	Telangana", "80, Bangalore	Karnataka"], columns = ['row'])
+print("DataFrame 'df':")
+print(df)
+# Split a text column into two separate columns
+df_out = df.row.str.split(',|\t', expand = True)
+
+# Make first as header
+new_header = df_out.iloc[0]
+df_out = df_out[1 :]
+df_out.columns = new_header
+print("\nSplit a text column into two separate columns:")
+print(df_out)
